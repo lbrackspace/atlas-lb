2a6dba27345febca12c920e7fa78abc0b9bea26f
Phillip Toohill <phillip.toohill@rackspace.com>
2011-07-26 10:07:56 -0500

    adding tests for sharing vip

--- persistence/src/test/java/org/openstack/atlas/service/domain/services/LoadBalancerServiceImplIntegrationTest.java
+++ persistence/src/test/java/org/openstack/atlas/service/domain/services/LoadBalancerServiceImplIntegrationTest.java
@@ -124,6 +124,7 @@ public class LoadBalancerServiceImplIntegrationTest {
             private EntityManager entityManager;
 
             private LoadBalancer loadBalancer;
+            private LoadBalancer loadBalancer2;
 
             @Before
             public void setUp() throws NoSuchAlgorithmException {
@@ -140,6 +141,20 @@ public class LoadBalancerServiceImplIntegrationTest {
                 node.setCondition(NodeCondition.ENABLED);
                 nodes.add(node);
                 loadBalancer.setNodes(nodes);
+
+                loadBalancer2 = new LoadBalancer();
+                loadBalancer2.setAccountId(10002);
+                loadBalancer2.setName("integration testing");
+                loadBalancer2.setPort(80);
+                loadBalancer2.setProtocol(LoadBalancerProtocol.POP3);
+
+                Set<Node> nodes2 = new HashSet<Node>();
+                Node node2 = new Node();
+                node.setIpAddress("2.2.2.2");
+                node.setPort(80);
+                node.setCondition(NodeCondition.ENABLED);
+                nodes.add(node2);
+                loadBalancer.setNodes(nodes2);
 
                 virtualIpService.addAccountRecord(1000);
 
@@ -290,6 +305,124 @@ public class LoadBalancerServiceImplIntegrationTest {
                 Assert.assertEquals(1, dbLoadBalancer.getLoadBalancerJoinVip6Set().size());
                 Assert.assertEquals(0, dbLoadBalancer.getLoadBalancerJoinVipSet().size());
             }
+
+            @Test
+            public void shouldReturnIpv4AndIpv6ForSharedV6() throws Exception {
+                //Build first loadbalancer
+                VirtualIpv6 virtualIpv6 = new VirtualIpv6();
+                org.openstack.atlas.service.domain.entities.VirtualIp virtualIp = new org.openstack.atlas.service.domain.entities.VirtualIp();
+                Set<LoadBalancerJoinVip> loadBalancerJoinVipSet = new HashSet<LoadBalancerJoinVip>();
+                LoadBalancerJoinVip loadBalancerJoinVip = new LoadBalancerJoinVip();
+                Set<LoadBalancerJoinVip6> loadBalancerJoinVip6Set = new HashSet<LoadBalancerJoinVip6>();
+                LoadBalancerJoinVip6 loadBalancerJoinVip6 = new LoadBalancerJoinVip6();
+
+                loadBalancerJoinVip.setVirtualIp(virtualIp);
+                loadBalancerJoinVip6.setVirtualIp(virtualIpv6);
+                loadBalancerJoinVipSet.add(loadBalancerJoinVip);
+                loadBalancerJoinVip6Set.add(loadBalancerJoinVip6);
+                VirtualIpDozerWrapper virtualIpDozerWrapper = new VirtualIpDozerWrapper(loadBalancerJoinVipSet, loadBalancerJoinVip6Set);
+                loadBalancer.setVirtualIpDozerWrapper(virtualIpDozerWrapper);
+                LoadBalancer dbLoadBalancer = loadBalancerService.create(loadBalancer);
+                LoadBalancer loadBalancer = loadBalancerService.get(dbLoadBalancer.getId(), dbLoadBalancer.getAccountId());
+
+                Assert.assertNotNull(loadBalancer);
+                for (LoadBalancerJoinVip lbjv : dbLoadBalancer.getLoadBalancerJoinVipSet()) {
+                    Assert.assertNotNull(lbjv.getVirtualIp().getId());
+                }
+
+                for (LoadBalancerJoinVip6 lbjv : dbLoadBalancer.getLoadBalancerJoinVip6Set()) {
+                    Assert.assertNotNull(lbjv.getVirtualIp().getId());
+                }
+
+                //Build second load balancer and share first load balancer's ipv6
+                VirtualIpv6 virtualIpv62 = new VirtualIpv6();
+                org.openstack.atlas.service.domain.entities.VirtualIp virtualIp2 = new org.openstack.atlas.service.domain.entities.VirtualIp();
+                Set<LoadBalancerJoinVip> loadBalancerJoinVipSet2 = new HashSet<LoadBalancerJoinVip>();
+                LoadBalancerJoinVip loadBalancerJoinVip2 = new LoadBalancerJoinVip();
+                Set<LoadBalancerJoinVip6> loadBalancerJoinVip6Set2 = new HashSet<LoadBalancerJoinVip6>();
+                LoadBalancerJoinVip6 loadBalancerJoinVip62 = new LoadBalancerJoinVip6();
+
+                loadBalancerJoinVip.setVirtualIp(virtualIp2);
+
+                for (LoadBalancerJoinVip6 jvip : dbLoadBalancer.getLoadBalancerJoinVip6Set()) {
+                    virtualIpv62.setId(jvip.getVirtualIp().getId());
+                    break;
+                }
+
+                loadBalancerJoinVip6.setVirtualIp(virtualIpv62);
+                loadBalancerJoinVipSet.add(loadBalancerJoinVip2);
+                loadBalancerJoinVip6Set.add(loadBalancerJoinVip62);
+                VirtualIpDozerWrapper virtualIpDozerWrapper2 = new VirtualIpDozerWrapper(loadBalancerJoinVipSet2, loadBalancerJoinVip6Set2);
+                loadBalancer2.setVirtualIpDozerWrapper(virtualIpDozerWrapper2);
+                LoadBalancer dbLoadBalancer2 = loadBalancerService.create(loadBalancer2);
+                LoadBalancer loadBalancer3 = loadBalancerService.get(dbLoadBalancer2.getId(), dbLoadBalancer2.getAccountId());
+
+                Assert.assertNotNull(loadBalancer3);
+                for (LoadBalancerJoinVip6 jvip : dbLoadBalancer.getLoadBalancerJoinVip6Set()) {
+                    for (LoadBalancerJoinVip6 jvip2 : dbLoadBalancer2.getLoadBalancerJoinVip6Set()) {
+                        Assert.assertEquals(jvip2.getVirtualIp().getId(), jvip.getVirtualIp().getId());
+                    }
+                }
+            }
+
+             @Test
+            public void shouldReturnIpv4AndIpv6ForSharedV64() throws Exception {
+                //Build first loadbalancer
+                VirtualIpv6 virtualIpv6 = new VirtualIpv6();
+                org.openstack.atlas.service.domain.entities.VirtualIp virtualIp = new org.openstack.atlas.service.domain.entities.VirtualIp();
+                Set<LoadBalancerJoinVip> loadBalancerJoinVipSet = new HashSet<LoadBalancerJoinVip>();
+                LoadBalancerJoinVip loadBalancerJoinVip = new LoadBalancerJoinVip();
+                Set<LoadBalancerJoinVip6> loadBalancerJoinVip6Set = new HashSet<LoadBalancerJoinVip6>();
+                LoadBalancerJoinVip6 loadBalancerJoinVip6 = new LoadBalancerJoinVip6();
+
+                loadBalancerJoinVip.setVirtualIp(virtualIp);
+                loadBalancerJoinVip6.setVirtualIp(virtualIpv6);
+                loadBalancerJoinVipSet.add(loadBalancerJoinVip);
+                loadBalancerJoinVip6Set.add(loadBalancerJoinVip6);
+                VirtualIpDozerWrapper virtualIpDozerWrapper = new VirtualIpDozerWrapper(loadBalancerJoinVipSet, loadBalancerJoinVip6Set);
+                loadBalancer.setVirtualIpDozerWrapper(virtualIpDozerWrapper);
+                LoadBalancer dbLoadBalancer = loadBalancerService.create(loadBalancer);
+                LoadBalancer loadBalancer = loadBalancerService.get(dbLoadBalancer.getId(), dbLoadBalancer.getAccountId());
+
+                Assert.assertNotNull(loadBalancer);
+                for (LoadBalancerJoinVip lbjv : dbLoadBalancer.getLoadBalancerJoinVipSet()) {
+                    Assert.assertNotNull(lbjv.getVirtualIp().getId());
+                }
+
+                for (LoadBalancerJoinVip6 lbjv : dbLoadBalancer.getLoadBalancerJoinVip6Set()) {
+                    Assert.assertNotNull(lbjv.getVirtualIp().getId());
+                }
+
+                //Build second load balancer and share first load balancer's ipv6
+                VirtualIpv6 virtualIpv62 = new VirtualIpv6();
+                org.openstack.atlas.service.domain.entities.VirtualIp virtualIp2 = new org.openstack.atlas.service.domain.entities.VirtualIp();
+                Set<LoadBalancerJoinVip> loadBalancerJoinVipSet2 = new HashSet<LoadBalancerJoinVip>();
+                LoadBalancerJoinVip loadBalancerJoinVip2 = new LoadBalancerJoinVip();
+                Set<LoadBalancerJoinVip6> loadBalancerJoinVip6Set2 = new HashSet<LoadBalancerJoinVip6>();
+                LoadBalancerJoinVip6 loadBalancerJoinVip62 = new LoadBalancerJoinVip6();
+
+                loadBalancerJoinVip.setVirtualIp(virtualIp2);
+
+                for (LoadBalancerJoinVip6 jvip : dbLoadBalancer.getLoadBalancerJoinVip6Set()) {
+                    virtualIpv62.setId(jvip.getVirtualIp().getId());
+                    break;
+                }
+
+                loadBalancerJoinVip6.setVirtualIp(virtualIpv62);
+                loadBalancerJoinVipSet.add(loadBalancerJoinVip2);
+                loadBalancerJoinVip6Set.add(loadBalancerJoinVip62);
+                VirtualIpDozerWrapper virtualIpDozerWrapper2 = new VirtualIpDozerWrapper(loadBalancerJoinVipSet2, loadBalancerJoinVip6Set2);
+                loadBalancer2.setVirtualIpDozerWrapper(virtualIpDozerWrapper2);
+                LoadBalancer dbLoadBalancer2 = loadBalancerService.create(loadBalancer2);
+                LoadBalancer loadBalancer3 = loadBalancerService.get(dbLoadBalancer2.getId(), dbLoadBalancer2.getAccountId());
+
+                Assert.assertNotNull(loadBalancer3);
+                for (LoadBalancerJoinVip jvip : dbLoadBalancer.getLoadBalancerJoinVipSet()) {
+                    for (LoadBalancerJoinVip jvip2 : dbLoadBalancer2.getLoadBalancerJoinVipSet()) {
+                        Assert.assertEquals(jvip2.getVirtualIp().getId(), jvip.getVirtualIp().getId());
+                    }
+                }
+            }
         }
 
         //TODO:Move..
@@ -391,4 +524,3 @@ public class LoadBalancerServiceImplIntegrationTest {
         }
     }
 }
-
