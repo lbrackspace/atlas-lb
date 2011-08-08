1e52b3d40a6695f8c5fb90b60201e788a2663b31
Sudarshan Acharya <sudarshan.acharya@rackspace.com>
2011-08-08 11:27:20 -0500

    Fixed the pom and added logging.

--- rax/api/common-api/src/main/java/org/openstack/atlas/rax/api/resources/LoadBalancersResource.java
+++ rax/api/common-api/src/main/java/org/openstack/atlas/rax/api/resources/LoadBalancersResource.java
@@ -3,10 +3,15 @@ package org.openstack.atlas.rax.api.resources;
 import org.openstack.atlas.docs.loadbalancers.api.v1.LoadBalancer;
 
 import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
 public class LoadBalancersResource extends org.openstack.atlas.api.resources.LoadBalancersResource {
+    Logger logger = Logger.getLogger("LoadBalancersResource");
+
     @Override
     public Response createLoadBalancer(LoadBalancer loadBalancer) {
-        return null;
+        logger.info("Entering rax createLoadBalancer: ");
+        return Response.status(Response.Status.OK).entity(loadBalancer).build();
+
     }
 }
