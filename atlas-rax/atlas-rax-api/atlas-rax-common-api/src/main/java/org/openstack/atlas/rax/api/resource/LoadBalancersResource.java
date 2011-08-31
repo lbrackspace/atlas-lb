2e8e44d8dfd09f7d3f217b437fd06ad54756e22b
Sudarshan Acharya <sudarshan.acharya@rackspace.com>
2011-08-31 16:57:27 -0500

    Cleaning up dom parsing for jaxb

--- atlas-rax/atlas-rax-api/atlas-rax-common-api/src/main/java/org/openstack/atlas/rax/api/resource/LoadBalancersResource.java
+++ atlas-rax/atlas-rax-api/atlas-rax-common-api/src/main/java/org/openstack/atlas/rax/api/resource/LoadBalancersResource.java
@@ -3,7 +3,6 @@ package org.openstack.atlas.rax.api.resource;
 import org.openstack.atlas.api.response.ResponseFactory;
 import org.openstack.atlas.api.v1.extensions.rax.AccessList;
 import org.openstack.atlas.api.v1.extensions.rax.NetworkItem;
-import org.openstack.atlas.api.v1.extensions.rax.ObjectFactory;
 import org.openstack.atlas.api.validation.context.HttpRequestType;
 import org.openstack.atlas.api.validation.result.ValidatorResult;
 import org.openstack.atlas.core.api.v1.LoadBalancer;
@@ -11,19 +10,12 @@ import org.openstack.atlas.rax.domain.entity.AccessListType;
 import org.openstack.atlas.rax.domain.entity.RaxLoadBalancer;
 import org.openstack.atlas.rax.domain.pojo.RaxMessageDataContainer;
 import org.openstack.atlas.service.domain.operation.Operation;
-import org.openstack.atlas.service.domain.pojo.MessageDataContainer;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.w3c.dom.Element;
 
 import javax.ws.rs.core.Response;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.namespace.QName;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,39 +24,14 @@ import java.util.logging.Logger;
 public class LoadBalancersResource extends org.openstack.atlas.api.resource.LoadBalancersResource {
     Logger logger = Logger.getLogger("LoadBalancersResource");
 
-//    @Override
+    @Override
     public Response createLoadBalancer(LoadBalancer loadBalancer) {
         logger.log(Level.INFO, "loadbalancer: " + loadBalancer);
 
-        AccessList accessList1 = (AccessList) getAnyElement(loadBalancer);
-        Set<org.openstack.atlas.rax.domain.entity.AccessList> accessLists = new HashSet<org.openstack.atlas.rax.domain.entity.AccessList>();
-        if (accessList1 == null) {
-            logger.log(Level.INFO, "No accesslist found");
-        } else {
-            loadBalancer.getAnies().add(accessList1);
-            for (NetworkItem networkItem1 : accessList1.getNetworkItems()) {
-                logger.log(Level.INFO, "Element Network Item1: " + networkItem1.getAddress() + " : " + networkItem1.getType());
-                org.openstack.atlas.rax.domain.entity.AccessList accessList = new org.openstack.atlas.rax.domain.entity.AccessList();
-                accessList.setIpAddress(networkItem1.getAddress());
-                accessList.setType(AccessListType.valueOf(networkItem1.getType().value()));
-                accessLists.add(accessList);
-            }
-        }
-        //loadBalancer.getOtherAttributes().put(new QName("http://docs.openstack.org/test", "gender"), "male");
-
-        String crazyName = "";
-        Map<QName, String> otherAttributes = loadBalancer.getOtherAttributes();
-        for (QName qname : otherAttributes.keySet()) {
-            String value = otherAttributes.get(qname);
-            String key = qname.getLocalPart();
-            logger.log(Level.INFO, "Attribute: " + key + " : " + value);
-            if(key.equalsIgnoreCase("crazyName")) {
-                crazyName = value;
-            }
-        }
+        AccessList _accessList = AnyObjectMapper.getAccessList(loadBalancer);
+        String crazyName = AnyObjectMapper.getCrazyName(loadBalancer);
 
         ValidatorResult result = validator.validate(loadBalancer, HttpRequestType.POST);
-
         if (!result.passedValidation()) {
             return ResponseFactory.getValidationFaultResponse(result);
         }
@@ -74,9 +41,10 @@ public class LoadBalancersResource extends org.openstack.atlas.api.resource.Load
             mappedLb.setAccountId(accountId);
 
             RaxLoadBalancer raxLoadBalancer = dozerMapper.map(mappedLb, RaxLoadBalancer.class);
-            raxLoadBalancer.setAccessLists(accessLists);
+            raxLoadBalancer.setAccessLists(getDomainAccessLists(_accessList));
             raxLoadBalancer.setCrazyName(crazyName);
 
+            //This call should be moved somewhere else
             virtualIpService.addAccountRecord(accountId);
 
             org.openstack.atlas.service.domain.entity.LoadBalancer newlyCreatedLb = loadbalancerService.create(raxLoadBalancer);
@@ -89,31 +57,20 @@ public class LoadBalancersResource extends org.openstack.atlas.api.resource.Load
         }
     }
 
-    private Object getAnyElement(LoadBalancer loadBalancer) {
-        List<Object> anies = loadBalancer.getAnies();
-        for (Object any : anies) {
-            logger.log(Level.INFO, "Class: " + any.getClass());
-            if (any instanceof Element) {
-                Element element = (Element) any;
-                try {
-                    JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName());
-                    Unmarshaller unmarshaller = jc.createUnmarshaller();
-                    Object o = unmarshaller.unmarshal(element);
-                    if (o instanceof AccessList) {
-                        AccessList list1 = (AccessList) o;
-                        List<NetworkItem> ns = list1.getNetworkItems();
-                        return list1;
-                    } else if (o instanceof JAXBElement) {
-                        JAXBElement<AccessList> jaxbElement = (JAXBElement) o;
-                        AccessList list1 = jaxbElement.getValue();
-                        List<NetworkItem> ns = list1.getNetworkItems();
-                        return list1;
-                    }
-                } catch (JAXBException e) {
-                    e.printStackTrace();
-                }
+    private Set<org.openstack.atlas.rax.domain.entity.AccessList> getDomainAccessLists(AccessList _accessList) {
+        //TODO : Use dozer mapper to map to AccessList Entity
+        Set<org.openstack.atlas.rax.domain.entity.AccessList> accessLists = new HashSet<org.openstack.atlas.rax.domain.entity.AccessList>();
+        if (_accessList == null) {
+            logger.log(Level.INFO, "No accesslist found");
+        } else {
+            for (NetworkItem _networkItem : _accessList.getNetworkItems()) {
+                logger.log(Level.INFO, "Element Network Item: " + _networkItem.getAddress() + " : " + _networkItem.getType());
+                org.openstack.atlas.rax.domain.entity.AccessList accessList = new org.openstack.atlas.rax.domain.entity.AccessList();
+                accessList.setIpAddress(_networkItem.getAddress());
+                accessList.setType(AccessListType.valueOf(_networkItem.getType().value()));
+                accessLists.add(accessList);
             }
         }
-        return null;
+        return accessLists;
     }
 }
