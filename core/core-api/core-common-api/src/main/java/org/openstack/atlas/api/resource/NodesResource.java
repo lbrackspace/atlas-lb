5f88d10ab750668e97951454d580914a334f1895
Phillip Toohill <phillip.toohill@rackspace.com>
2011-10-05 09:47:12 -0500

    Updating README, begining add node

--- core/core-api/core-common-api/src/main/java/org/openstack/atlas/api/resource/NodesResource.java
+++ core/core-api/core-common-api/src/main/java/org/openstack/atlas/api/resource/NodesResource.java
@@ -1,11 +1,17 @@
 package org.openstack.atlas.api.resource;
 
 import org.apache.log4j.Logger;
+import org.openstack.atlas.api.resource.provider.CommonDependencyProvider;
 import org.openstack.atlas.api.response.ResponseFactory;
 import org.openstack.atlas.api.validation.context.HttpRequestType;
 import org.openstack.atlas.api.validation.result.ValidatorResult;
 import org.openstack.atlas.api.validation.validator.NodesValidator;
 import org.openstack.atlas.core.api.v1.Nodes;
+import org.openstack.atlas.service.domain.entity.LoadBalancer;
+import org.openstack.atlas.service.domain.entity.Node;
+import org.openstack.atlas.service.domain.operation.Operation;
+import org.openstack.atlas.service.domain.pojo.MessageDataContainer;
+import org.openstack.atlas.service.domain.service.NodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -13,12 +19,13 @@ import org.springframework.stereotype.Controller;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Set;
 
 import static javax.ws.rs.core.MediaType.*;
 
 @Controller
 @Scope("request")
-public class NodesResource {
+public class NodesResource extends CommonDependencyProvider {
     private final Logger LOG = Logger.getLogger(NodesResource.class);
     protected Integer accountId;
     protected Integer loadBalancerId;
@@ -27,6 +34,8 @@ public class NodesResource {
     protected NodesValidator validator;
     @Autowired
     private NodeResource nodeResource;
+    @Autowired
+    protected NodeService nodeService;
 
     @POST
     @Consumes({APPLICATION_XML, APPLICATION_JSON})
@@ -38,6 +47,18 @@ public class NodesResource {
         }
 
         try {
+             org.openstack.atlas.service.domain.entity.LoadBalancer apiLb = new org.openstack.atlas.service.domain.entity.LoadBalancer();
+            apiLb.getNodes().addAll(_nodes.getNodes());
+            LoadBalancer domainLb = dozerMapper.map(apiLb, LoadBalancer.class);
+            domainLb.setId(loadBalancerId);
+            domainLb.setAccountId(accountId);
+
+            Nodes returnNodes = new Nodes();
+            Set<Node> dbnodes = nodeService.createNodes(domainLb);
+            for (Node node : dbnodes) {
+                returnNodes.getNodes().add(dozerMapper.map(node, org.openstack.atlas.docs.loadbalancers.api.v1.Node.class));
+            }
+            asyncService.callAsyncLoadBalancingOperation(Operation.CREATE_NODES, domainLb);
             // TODO: Implement
             return Response.status(Response.Status.ACCEPTED).entity("Return something useful!").build();
         } catch (Exception e) {
--- core/core-persistence/src/main/java/org/openstack/atlas/service/domain/service/impl/NodeServiceImpl.java
+++ core/core-persistence/src/main/java/org/openstack/atlas/service/domain/service/impl/NodeServiceImpl.java
@@ -2,10 +2,81 @@ package org.openstack.atlas.service.domain.service.impl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openstack.atlas.service.domain.entity.LoadBalancer;
+import org.openstack.atlas.service.domain.entity.Node;
+import org.openstack.atlas.service.domain.exception.EntityNotFoundException;
+import org.openstack.atlas.service.domain.exception.ImmutableEntityException;
+import org.openstack.atlas.service.domain.exception.UnprocessableEntityException;
+import org.openstack.atlas.service.domain.repository.LoadBalancerRepository;
+import org.openstack.atlas.service.domain.repository.NodeRepository;
 import org.openstack.atlas.service.domain.service.NodeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 @Service
 public class NodeServiceImpl implements NodeService {
     private final Log LOG = LogFactory.getLog(NodeServiceImpl.class);
+
+    @Autowired
+    protected NodeRepository nodeRepository;
+
+    @Autowired
+    protected LoadBalancerRepository loadBalancerRepository;
+
+//    @Override
+//    @Transactional
+//    public Set<Node> createNodes(LoadBalancer loadBalancer) throws EntityNotFoundException, ImmutableEntityException, UnprocessableEntityException, BadRequestException {
+//        LoadBalancer dbLoadBalancer = loadBalancerRepository.getByIdAndAccountId(loadBalancer.getId(), loadBalancer.getAccountId());
+//        isLbActive(dbLoadBalancer);
+//
+//        Integer potentialTotalNumNodes = dbLoadBalancer.getNodes().size() + loadBalancer.getNodes().size();
+//        Integer nodeLimit = accountLimitService.getLimit(dbLoadBalancer.getAccountId(), AccountLimitType.NODE_LIMIT);
+//
+//        if (potentialTotalNumNodes > nodeLimit) {
+//            throw new BadRequestException(String.format("Nodes must not exceed %d per load balancer.", nodeLimit));
+//        }
+//
+//        LOG.debug("Verifying that there are no duplicate nodes...");
+//        if (detectDuplicateNodes(dbLoadBalancer, loadBalancer)) {
+//            LOG.warn("Duplicate nodes found! Sending failure response back to client...");
+//            throw new UnprocessableEntityException("Duplicate nodes detected. One or more nodes already configured on load balancer.");
+//        }
+//
+//        if (!areAddressesValidForUse(loadBalancer.getNodes(), dbLoadBalancer)) {
+//            LOG.warn("Internal Ips found! Sending failure response back to client...");
+//            throw new BadRequestException("Invalid node address. The load balancer's virtual ip or host end point address cannot be used as a node address.");
+//        }
+//
+//        try {
+//            Node badNode = blackListedItemNode(loadBalancer.getNodes());
+//            if (badNode != null) {
+//                throw new BadRequestException(String.format("Invalid node address. The address '%s' is currently not accepted for this request.", badNode.getIpAddress()));
+//            }
+//        } catch (IPStringConversionException ipe) {
+//            LOG.warn("IPStringConversionException thrown. Sending error response to client...");
+//            throw new BadRequestException("IP address was not converted properly, we are unable to process this request.");
+//        } catch (IpTypeMissMatchException ipte) {
+//            LOG.warn("EntityNotFoundException thrown. Sending error response to client...");
+//            throw new BadRequestException("IP addresses type are mismatched, we are unable to process this request.");
+//        }
+//
+//        LOG.debug("Updating the lb status to pending_update");
+//        dbLoadBalancer.setStatus(LoadBalancerStatus.PENDING_UPDATE);
+//
+//        LOG.debug("Current number of nodes for loadbalancer: " + dbLoadBalancer.getNodes().size());
+//        LOG.debug("Number of new nodes to be added: " + loadBalancer.getNodes().size());
+//        NodesHelper.setNodesToStatus(loadBalancer, NodeStatus.ONLINE);
+//
+//        for (Node newNode : loadBalancer.getNodes()) {
+//            if (newNode.getWeight() == null) {
+//                newNode.setWeight(1);
+//            }
+//        }
+//
+//        return nodeRepository.addNodes(dbLoadBalancer, loadBalancer.getNodes());
+//    }
+
 }
