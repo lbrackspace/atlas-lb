cef3a72839e750ea8ae1495738ef295396721a85
Uma Goring <uma.goring@M1BRAGY.rackspace.corp>
2011-10-05 14:17:23 -0500

    added core delete session persistence call

---
+++ core/core-api/core-common-api/src/main/java/org/openstack/atlas/api/async/DeleteSessionPersistenceListener.java
@@ -0,0 +1,82 @@
+package org.openstack.atlas.api.async;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openstack.atlas.datamodel.CoreLoadBalancerStatus;
+import org.openstack.atlas.service.domain.entity.LoadBalancer;
+import org.openstack.atlas.service.domain.exception.EntityNotFoundException;
+import org.openstack.atlas.service.domain.repository.LoadBalancerRepository;
+import org.openstack.atlas.service.domain.service.LoadBalancerService;
+import org.openstack.atlas.service.domain.service.NotificationService;
+import org.openstack.atlas.service.domain.service.SessionPersistenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+import static org.openstack.atlas.service.domain.common.AlertType.DATABASE_FAILURE;
+import static org.openstack.atlas.service.domain.common.AlertType.LBDEVICE_FAILURE;
+import static org.openstack.atlas.service.domain.event.entity.CategoryType.DELETE;
+import static org.openstack.atlas.service.domain.event.entity.EventSeverity.CRITICAL;
+import static org.openstack.atlas.service.domain.event.entity.EventSeverity.INFO;
+import static org.openstack.atlas.service.domain.event.entity.EventType.DELETE_SESSION_PERSISTENCE;
+
+@Component
+public class DeleteSessionPersistenceListener extends BaseListener {
+    private final Log LOG = LogFactory.getLog(DeleteSessionPersistenceListener.class);
+
+    @Autowired
+    private SessionPersistenceService sessionPersistenceService;
+     @Autowired
+    private LoadBalancerService loadBalancerService;
+    @Autowired
+    private NotificationService notificationService;
+    @Autowired
+    private LoadBalancerRepository loadBalancerRepository;
+
+    @Override
+    public void doOnMessage(final Message message) throws Exception {
+        LOG.debug("Entering " + getClass());
+        LOG.debug(message);
+
+        LoadBalancer queueLb = getDataContainerFromMessage(message).getLoadBalancer();
+        LoadBalancer dbLoadBalancer;
+
+        try {
+            dbLoadBalancer = loadBalancerRepository.getByIdAndAccountId(queueLb.getId(), queueLb.getAccountId());
+        } catch (EntityNotFoundException enfe) {
+            String alertDescription = String.format("Load balancer '%d' not found in database.", queueLb.getId());
+            LOG.error(alertDescription, enfe);
+            notificationService.saveAlert(queueLb.getAccountId(), queueLb.getId(), enfe, DATABASE_FAILURE.name(), alertDescription);
+            sendErrorToEventResource(queueLb);
+            return;
+        }
+
+        try {
+                    LOG.debug(String.format("Removing session persistence for load balancer '%d' in LB Device...", queueLb.getId()));
+                    reverseProxyLoadBalancerService.deleteSessionPersistence(queueLb.getAccountId(), queueLb.getId());
+                    LOG.debug(String.format("Successfully removed session persistence for load balancer '%d' in LB Device.", queueLb.getId()));
+                } catch (Exception e) {
+                    loadBalancerRepository.changeStatus(dbLoadBalancer, CoreLoadBalancerStatus.ERROR);
+                    String alertDescription = String.format("Error removing session persistence in LB Device for loadbalancer '%d'.", queueLb.getId());
+                    LOG.error(alertDescription, e);
+                    notificationService.saveAlert(queueLb.getAccountId(), queueLb.getId(), e, LBDEVICE_FAILURE.name(), alertDescription);
+                    sendErrorToEventResource(queueLb);
+                    return;
+                }
+
+                 loadBalancerRepository.changeStatus(dbLoadBalancer, CoreLoadBalancerStatus.ACTIVE);
+                // Add atom entry
+                String atomTitle = "Session Persistence Successfully Deleted";
+                String atomSummary = "Session persistence successfully deleted";
+                notificationService.saveSessionPersistenceEvent(queueLb.getUserName(), dbLoadBalancer.getAccountId(), dbLoadBalancer.getId(), atomTitle, atomSummary, DELETE_SESSION_PERSISTENCE, DELETE, INFO);
+
+                LOG.info(String.format("Delete session persistence operation complete for load balancer '%d'.", queueLb.getId()));
+            }
+
+            private void sendErrorToEventResource(LoadBalancer lb) {
+                String title = "Error Deleting Session Persistence";
+                String desc = "Could not delete the session persistence settings at this time.";
+                notificationService.saveSessionPersistenceEvent(lb.getUserName(), lb.getAccountId(), lb.getId(), title, desc, DELETE_SESSION_PERSISTENCE, DELETE, CRITICAL);
+            }
+        }
