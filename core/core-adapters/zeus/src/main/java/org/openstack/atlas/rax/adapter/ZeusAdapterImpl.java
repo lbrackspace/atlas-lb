f6a0a87405a6e55304ed5a8400368b29738f0682
Sudarshan Acharya <sudarshan.acharya@rackspace.com>
2011-10-18 17:41:51 -0500

    Added ability for  switching adapters from config file.

--- core/core-adapters/zeus/src/main/java/org/openstack/atlas/rax/adapter/ZeusAdapterImpl.java
+++ core/core-adapters/zeus/src/main/java/org/openstack/atlas/rax/adapter/ZeusAdapterImpl.java
@@ -1,7 +1,9 @@
-package org.openstack.atlas.adapter;
+package org.openstack.atlas.rax.adapter;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openstack.atlas.adapter.LoadBalancerAdapter;
+import org.openstack.atlas.adapter.LoadBalancerEndpointConfiguration;
 import org.openstack.atlas.adapter.exception.AdapterException;
 import org.openstack.atlas.service.domain.entity.*;
 import org.springframework.stereotype.Service;
@@ -9,77 +11,62 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class NullAdapterImpl implements LoadBalancerAdapter {
-    public static Log LOG = LogFactory.getLog(NullAdapterImpl.class.getName());
+public class ZeusAdapterImpl implements LoadBalancerAdapter {
+    public static Log LOG = LogFactory.getLog(ZeusAdapterImpl.class.getName());
 
     @Override
     public void createLoadBalancer(LoadBalancerEndpointConfiguration config, Integer accountId, LoadBalancer lb) throws AdapterException {
-        LOG.info("createLoadBalancer"); // NOP
     }
 
     @Override
     public void updateLoadBalancer(LoadBalancerEndpointConfiguration config, Integer accountId, LoadBalancer lb) throws AdapterException {
-        LOG.info("updateLoadBalancer");// NOP
     }
 
     @Override
     public void deleteLoadBalancer(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId) throws AdapterException {
-        LOG.info("deleteLoadBalancer");// NOP
     }
 
     @Override
     public void createNodes(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId, Set<Node> nodes) throws AdapterException {
-        LOG.info("createNodes");// NOP
     }
 
     @Override
     public void deleteNodes(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId, Set<Integer> nodeIds) throws AdapterException {
-        LOG.info("deleteNodes");// NOP
     }
 
     @Override
     public void updateNode(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId, Node node) throws AdapterException {
-        LOG.info("updateNodes");// NOP
     }
 
     @Override
     public void deleteNode(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId, Integer nodeId) throws AdapterException {
-        LOG.info("deleteNode");// NOP
     }
 
     @Override
     public void updateConnectionLogging(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId, Boolean enabled) throws AdapterException {
-        LOG.info("updateConnectionLogging");// NOP
     }
 
     @Override
     public void updateConnectionThrottle(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId, ConnectionThrottle connectionThrottle) throws AdapterException {
-        LOG.info("updateConnectionThrottle");// NOP
     }
 
     @Override
     public void deleteConnectionThrottle(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId) throws AdapterException {
-        LOG.info("deleteConnectionThrottle");// NOP
     }
 
     @Override
     public void updateHealthMonitor(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId, HealthMonitor monitor) throws AdapterException {
-        LOG.info("updateHealthMonitor");// NOP
     }
 
     @Override
     public void deleteHealthMonitor(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId) throws AdapterException {
-        LOG.info("deleteHealthMonitor");// NOP
     }
 
     @Override
     public void setSessionPersistence(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId, SessionPersistence sessionPersistence) throws AdapterException {
-        LOG.info("setSessionPersistence");// NOP
     }
 
     @Override
     public void deleteSessionPersistence(LoadBalancerEndpointConfiguration config, Integer accountId, Integer lbId) throws AdapterException {
-        LOG.info("deleteSessionPersistence");// NOP
     }
-
 }
