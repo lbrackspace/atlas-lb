d94cc632f8b1047be92b337376df8c2bdea3178a
Sudarshan Acharya <sudarshan.acharya@rackspace.com>
2011-10-24 15:29:42 -0500

    Adding the Service annotation to ZXTM Adapter

--- core/core-adapters/core-zxtm-adapter/src/main/java/org/openstack/atlas/adapter/zxtm/ZxtmAdapterImpl.java
+++ core/core-adapters/core-zxtm-adapter/src/main/java/org/openstack/atlas/adapter/zxtm/ZxtmAdapterImpl.java
@@ -18,6 +18,7 @@ import org.openstack.atlas.datamodel.CoreHealthMonitorType;
 import org.openstack.atlas.datamodel.CorePersistenceType;
 import org.openstack.atlas.datamodel.CoreProtocolType;
 import org.openstack.atlas.service.domain.entity.*;
+import org.springframework.stereotype.Service;
 
 import java.rmi.RemoteException;
 import java.util.*;
