834fafedd4283b0235bf12912bc92e5c595d92d9
Uma Goring <uma.goring@M1BRAGY.rackspace.corp>
2011-11-14 16:45:59 -0600

    fixed bug for role support for activeratelimit and custom limit

--- api/mgmt-web/src/main/java/org/openstack/atlas/api/mgmt/resources/AccountLimitsResource.java
+++ api/mgmt-web/src/main/java/org/openstack/atlas/api/mgmt/resources/AccountLimitsResource.java
@@ -21,7 +21,7 @@ public class AccountLimitsResource extends ManagementDependencyProvider {
 
     @GET
     public Response retrieveAllLimitsForAccount(@QueryParam("offset") Integer offset, @QueryParam("limit") Integer limit, @QueryParam("marker") Integer marker) {
-        if (!isUserInRole("cp, ops")) {
+        if (!isUserInRole("cp,ops")) {
             return ResponseFactory.accessDenied();
         }
 
