8c5be114a06302f2f09a0b2dca6ee36547a32ea2
Carlos D. Garza <carlos.garza@rackspace.com>
2012-02-10 11:19:27 -0600

    I guess we need to use InnoDB for the node_type table foreign keys

--- migration/schema/loadbalancing/34-35.sql
+++ migration/schema/loadbalancing/34-35.sql
@@ -3,7 +3,7 @@ use loadbalancing;
 create table node_type(
     name varchar(16),
     primary key(name)
-);
+)engine=InnoDB DEFAULT CHARSET=utf8;
 
 insert into node_type(name)values("PRIMARY"),("SECONDARY");
 alter table node add column type varchar(32) not null default "PRIMARY";
