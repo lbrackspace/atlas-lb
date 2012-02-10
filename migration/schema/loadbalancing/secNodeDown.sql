fe417369f9193f827bb922b7cc3cb49b78cb41db
Carlos D. Garza <carlos.garza@rackspace.com>
2012-02-10 11:32:13 -0600

    Renameing 33,35 migratios to secNodes

--- migration/schema/loadbalancing/secNodeDown.sql
+++ migration/schema/loadbalancing/secNodeDown.sql
@@ -4,4 +4,3 @@ alter table node drop foreign key fk_type2node_typename;
 alter table node drop column type;
 drop table node_type;
 alter table loadbalancer drop index account_id;
-update `meta` set `meta_value` = '34' where `meta_key`='version';
--- migration/schema/loadbalancing/secNodeUp.sql
+++ migration/schema/loadbalancing/secNodeUp.sql
@@ -9,4 +9,3 @@ insert into node_type(name)values("PRIMARY"),("SECONDARY");
 alter table node add column type varchar(32) not null default "PRIMARY";
 alter table node add constraint fk_type2node_typename foreign key  (type) references node_type(name);
 alter table loadbalancer add index(account_id);
-update `meta` set `meta_value` = '35' where `meta_key`='version';
