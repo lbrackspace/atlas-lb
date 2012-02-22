3ee5b601c5939ea91e70397d703421fb3e48ee8b
Trevor Vardeman <trevor.vardeman@rackspace.com>
2012-02-22 14:02:15 -0600

    Added limit name checks in the validator, and appropriate error trapping for delete/put call on invalid ids

--- api/mgmt-web/src/main/java/org/openstack/atlas/api/mgmt/validation/validators/AccountLimitValidator.java
+++ api/mgmt-web/src/main/java/org/openstack/atlas/api/mgmt/validation/validators/AccountLimitValidator.java
@@ -1,18 +1,19 @@
 package org.openstack.atlas.api.mgmt.validation.validators;
 
-import org.openstack.atlas.docs.loadbalancers.api.management.v1.Limit;
-import org.openstack.atlas.docs.loadbalancers.api.management.v1.RateLimit;
-import org.openstack.atlas.api.validation.validators.ResourceValidator;
-import org.openstack.atlas.api.validation.validators.ValidatorUtilities;
-import org.openstack.atlas.api.validation.verifiers.MustBeNonNegativeInteger;
 import org.openstack.atlas.api.validation.Validator;
 import org.openstack.atlas.api.validation.ValidatorBuilder;
 import org.openstack.atlas.api.validation.results.ValidatorResult;
+import org.openstack.atlas.api.validation.validators.ResourceValidator;
+import org.openstack.atlas.api.validation.validators.ValidatorUtilities;
 import org.openstack.atlas.api.validation.verifiers.MustBeNonNegativeInteger;
+import org.openstack.atlas.api.validation.verifiers.Verifier;
+import org.openstack.atlas.api.validation.verifiers.VerifierResult;
+import org.openstack.atlas.docs.loadbalancers.api.management.v1.Limit;
+import org.openstack.atlas.service.domain.entities.AccountLimitType;
 
+import static org.openstack.atlas.api.validation.ValidatorBuilder.build;
 import static org.openstack.atlas.api.validation.context.HttpRequestType.POST;
 import static org.openstack.atlas.api.validation.context.HttpRequestType.PUT;
-import static org.openstack.atlas.api.validation.ValidatorBuilder.build;
 
 public class AccountLimitValidator implements ResourceValidator<Limit> {
 
@@ -23,6 +24,24 @@ public class AccountLimitValidator implements ResourceValidator<Limit> {
             {
                 // POST EXPECTATIONS
                 result(validationTarget().getName()).must().exist().forContext(POST).withMessage("Must provide a valid limit name.");
+                result(validationTarget().getName()).must().adhereTo(new Verifier<String>() {
+                    @Override
+                    public VerifierResult verify(String obj) {
+                        VerifierResult result = new VerifierResult(false);
+                        for (AccountLimitType type : AccountLimitType.values()) {
+                            try {
+                                result = new VerifierResult(type.equals(AccountLimitType.valueOf(obj)));
+                            } catch (Exception e) {
+                                return result;
+                            }
+                            if (result.passed()) {
+                                break;
+                            }
+                        }
+                        return result;
+                    }
+
+                }).forContext(POST).withMessage("Must provide a valid limit name.");
                 result(validationTarget().getValue()).must().adhereTo(new MustBeNonNegativeInteger()).forContext(POST).withMessage("Must provide a valid limit value.");
 
                 // PUT EXPECTATIONS
