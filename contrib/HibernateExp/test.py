72fac97312607d25b12d0bc7cbc45e6b2af73419
Carlos D. Garza <carlos.garza@rackspace.com>
2012-02-29 19:30:40 -0600

    bumping HibernateExp pom version.

--- contrib/HibernateExp/test.py
+++ contrib/HibernateExp/test.py
@@ -2,19 +2,17 @@
 	
 import com.zxtm.service.client.CertificateFiles as CertificateFiles
 import util
-util.setConfig("slice.json")
+util.setConfig("ndev.json",skipDb=True)
 from util import *
 
-begin()
-nds = qq("SELECT ad from AllowedDomains ad")
+s = ZeusTest(stubs)
 
+nodes = [["50.56.125.120:80","184.106.70.249:80"]]
+pools = ["354934_211"]
 
+tmp = s.stubs.p.getNodesPriorityValue(pools,nodes)
+pris = [(p.getNode(),p.getPriority()) for p in tmp[0]]
 
-ro = AllowedDomains()
-ro.setName("rackexp.org")
-vn = AllowedDomains()
-vn.setName("viralnotes.com")
-tn = AllowedDomains()
-tn.setName("thesenotions.com")
+penabled = s.stubs.p.getPriorityEnabled(pools)
 
-saveList([ro,vn,tn])
+pnodes = s.stubs.p.getPriorityNodes(pools)
