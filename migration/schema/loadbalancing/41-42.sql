0ed42f7ee4caedbe996e314cd4079c08e982ffc6
Phillip Toohill <phillip.toohill@rackspace.com>
2012-05-21 10:03:34 -0500

    resolving issue with contentcaching throwing npe

--- migration/schema/loadbalancing/41-42.sql
+++ migration/schema/loadbalancing/41-42.sql
@@ -20,7 +20,7 @@ CREATE TABLE `node_meta_data` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-alter table `loadbalancer` add column `content_caching` tinyint(1) NULL default '0';
+alter table `loadbalancer` add column `content_caching` tinyint(1) NOT NULL default '0';
 insert into `event_type` values("UPDATE_CONTENT_CACHING", "Update Content Caching");
 
 update `meta` set `meta_value` = '42' where `meta_key`='version';
