96b2998945db5ad7141fdd028ecb6c9ece387bcc
Carlos D. Garza <crc@bork.rackexp.org>
2012-09-17 12:35:51 -0500

    updating migration schema.

--- migration/schema/loadbalancing/46-47.sql
+++ migration/schema/loadbalancing/46-47.sql
@@ -4,3 +4,4 @@ alter table loadbalancer_service_event add index created_idx(created);
 alter table loadbalancer_service_event add index account_id_idx(account_id);
 alter table loadbalancer_service_event add index loadbalancer_id_idx(loadbalancer_id);
 
+update `meta` set `meta_value` = '47' where `meta_key`='version';
