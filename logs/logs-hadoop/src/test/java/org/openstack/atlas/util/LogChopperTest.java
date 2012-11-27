e05858aaac65ec76daebc4323292544ba6aaa0b8
Phillip Toohill <phillip.toohill@rackspace.com>
2012-11-27 16:33:21 -0600

    update logging to handle ip

--- logs/logs-hadoop/src/test/java/org/openstack/atlas/util/LogChopperTest.java
+++ logs/logs-hadoop/src/test/java/org/openstack/atlas/util/LogChopperTest.java
@@ -2,10 +2,8 @@ package org.openstack.atlas.util;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openstack.atlas.io.LbLogsWritable;
-import org.openstack.atlas.util.LogChopper;
 
 public class LogChopperTest extends TestCase {
 
@@ -39,5 +37,33 @@ public class LogChopperTest extends TestCase {
         String testLineWithOutSSL = "554867_15815 toolbar.lavasoft.com 213.184.198.10 - - [27/Jan/2012:07:41:37 +0000] \"GET /malwaresitelist/data/120126162223-l.zip HTTP/1.0\" 200 1136 \"-\" \"SimpleGet\"";
         LbLogsWritable logsWritableWithSSL = LogChopper.getLbLogStats(testLineWithOutSSL);
         Assert.assertEquals(testLineWithOutSSL, logsWritableWithSSL.getLogline());
+    }
+
+    @Test
+    public void testHTTPWithIP() throws Exception {
+        String testLineWithIP = "406271_37533 50.56.48.198 46.137.248.139 - - [21/Nov/2012:03:51:45 +0000] \"GET /phpMyAdmin/translators.html HTTP/1.1\" 404 544 \"-\" \"Mozilla/4.0 (compatible; MSIE 6.0; MSIE 5.5; Windows NT 5.1) Opera 7.01 [en]\" 10.1.1.1:80";
+        LbLogsWritable logsWritableWithip = LogChopper.getLbLogStats(testLineWithIP);
+        Assert.assertEquals(testLineWithIP, logsWritableWithip.getLogline());
+    }
+
+    @Test
+    public void testHTTPWithIPSimple() throws Exception {
+        String testLineWithIP = "554867_15815 toolbar.lavasoft.com 213.184.198.10 - - [27/Jan/2012:07:41:37 +0000] \"GET /malwaresitelist/data/120126162223-l.zip HTTP/1.0\" 200 1136 \"-\" \"SimpleGet\" 10.1.1.1";
+        LbLogsWritable logsWritableWithip = LogChopper.getLbLogStats(testLineWithIP);
+        Assert.assertEquals(testLineWithIP, logsWritableWithip.getLogline());
+    }
+
+    @Test
+    public void testHTTPWithIPV6() throws Exception {
+        String testLineWithIv6P = "554867_15815 toolbar.lavasoft.com 213.184.198.10 - - [27/Jan/2012:07:41:37 +0000] \"GET /malwaresitelist/data/120126162223-l.zip HTTP/1.0\" 200 1136 \"-\" \"SimpleGet\" 2607:f0d0:1002:0051:0000:0000:0000:0004";
+        LbLogsWritable logsWritableWithip = LogChopper.getLbLogStats(testLineWithIv6P);
+        Assert.assertEquals(testLineWithIv6P, logsWritableWithip.getLogline());
+    }
+
+    @Test
+    public void testHTTPWithIPV6Compression() throws Exception {
+        String testLineWithIv6P = "554867_15815 toolbar.lavasoft.com 213.184.198.10 - - [27/Jan/2012:07:41:37 +0000] \"GET /malwaresitelist/data/120126162223-l.zip HTTP/1.0\" 200 1136 \"-\" \"SimpleGet\" 2607:f0d0:1002:51::4";
+        LbLogsWritable logsWritableWithip = LogChopper.getLbLogStats(testLineWithIv6P);
+        Assert.assertEquals(testLineWithIv6P, logsWritableWithip.getLogline());
     }
 }
