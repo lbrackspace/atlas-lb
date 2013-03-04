2cd80b0aea0e6a3b8abad150040a7af9d8869989
Phillip Toohill <phillip.toohill@rackspace.com>
2013-03-04 15:59:09 -0600

    Updating CloudFilesDAOImpl for temporary fix of terminated/disabled users and clogging valid file uploads

--- logs/logs-web/src/main/java/org/openstack/atlas/cloudfiles/CloudFilesDaoImpl.java
+++ logs/logs-web/src/main/java/org/openstack/atlas/cloudfiles/CloudFilesDaoImpl.java
@@ -7,12 +7,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openstack.atlas.auth.AuthUser;
 import org.openstack.atlas.util.FileSystemUtils;
+import org.openstack.atlas.util.StaticFileUtils;
 import org.springframework.beans.factory.annotation.Required;
 
 import javax.activation.FileTypeMap;
 import java.io.File;
 import java.util.HashMap;
-import org.openstack.atlas.util.StaticFileUtils;
 
 public class CloudFilesDaoImpl implements CloudFilesDao {
 
@@ -34,45 +34,50 @@ public class CloudFilesDaoImpl implements CloudFilesDao {
         if (!localFile.exists()) {
             throw new FilesException("Could not find the local file  " + localFileName + " anymore.", null);
         }
-        client = new FilesClient(user.getUsername(), user.getAuthKey(), user.getCloudFilesAuthUrl(), null, 5000);
-        client.setAuthenticationURL(user.getCloudFilesAuthUrl());
 
-        String fullFilename = localFileName.replaceAll("\\.\\./", "./");
-        // the localFilename will always be before the last slash, if we are using
-        // slashes (directories)
-        String restOfFilename = StaticFileUtils.stripDirectoryFromFileName(fullFilename);
+        if (user.isEnabled()) {
+            client = new FilesClient(user.getUsername(), user.getAuthKey(), user.getCloudFilesAuthUrl(), null, 5000);
+            client.setAuthenticationURL(user.getCloudFilesAuthUrl());
 
-        try {
+            String fullFilename = localFileName.replaceAll("\\.\\./", "./");
+            // the localFilename will always be before the last slash, if we are using
+            // slashes (directories)
+            String restOfFilename = StaticFileUtils.stripDirectoryFromFileName(fullFilename);
+
             try {
-                client.login();
-            } catch (Exception e) {
-                // try again to log in, sometimes this fails randomly
-                client.login();
-            }
-            client.setCurrentRegion(user.getRegion());
-            if (!client.containerExists(containerName)) {
-                client.createContainer(containerName);
-            }
-            try {
-                if (client.getObjectMetaData(containerName, restOfFilename) != null) {
-                    throw new FilesException("Could not add file [" + localFileName + "]", null);
+                try {
+                    client.login();
+                } catch (Exception e) {
+                    // try again to log in, sometimes this fails randomly
+                    client.login();
                 }
-            } catch (FilesNotFoundException e) {
-                //file does nto exist, good.
-            }
-            File file = new File(fullFilename);
-            String contentType = fileMap.getContentType(file);
+                client.setCurrentRegion(user.getRegion());
+                if (!client.containerExists(containerName)) {
+                    client.createContainer(containerName);
+                }
+                try {
+                    if (client.getObjectMetaData(containerName, restOfFilename) != null) {
+                        throw new FilesException("Could not add file [" + localFileName + "]", null);
+                    }
+                } catch (FilesNotFoundException e) {
+                    //file does nto exist, good.
+                }
+                File file = new File(fullFilename);
+                String contentType = fileMap.getContentType(file);
 
-            try {
-                client.storeObjectAs(containerName, file, contentType, remoteFileName, new HashMap());
+                try {
+                    client.storeObjectAs(containerName, file, contentType, remoteFileName, new HashMap());
+                } catch (Exception e) {
+                    // Files has a tendency to crap out on uploads. Just try again.
+                    client.storeObjectAs(containerName, file, contentType, remoteFileName, new HashMap());
+                }
+
+
             } catch (Exception e) {
-                // Files has a tendency to crap out on uploads. Just try again.
-                client.storeObjectAs(containerName, file, contentType, remoteFileName, new HashMap());
+                throw new FilesException(fullFilename + "-Failed to upload file:" + e.getMessage(), e);
             }
-
-
-        } catch (Exception e) {
-            throw new FilesException(fullFilename + "-Failed to upload file:" + e.getMessage(), e);
+        } else {
+          LOG.info(String.format("User: %s is not enabled and logs will not be processed...", user.getUsername()));
         }
     }
 
