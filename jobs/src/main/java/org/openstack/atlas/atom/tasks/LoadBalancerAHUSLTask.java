ff5bf04f99d8cdbe95fad8eb27760a0bd8e8a3a4
Phillip Toohill <phillip.toohill@rackspace.com>
2013-03-18 22:56:35 -0500

    Updating B-42639 :: account for DELETE event and AHUSL rules..

--- jobs/src/main/java/org/openstack/atlas/atom/tasks/LoadBalancerAHUSLTask.java
+++ jobs/src/main/java/org/openstack/atlas/atom/tasks/LoadBalancerAHUSLTask.java
@@ -4,6 +4,7 @@ import com.sun.jersey.api.client.ClientHandlerException;
 import com.sun.jersey.api.client.ClientResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpStatus;
 import org.apache.http.conn.ConnectionPoolTimeoutException;
 import org.openstack.atlas.atom.client.AtomHopperClientImpl;
 import org.openstack.atlas.atom.config.AtomHopperConfiguration;
@@ -13,6 +14,7 @@ import org.openstack.atlas.atom.factory.UsageEntryFactoryImpl;
 import org.openstack.atlas.atom.util.AHUSLUtil;
 import org.openstack.atlas.cfg.Configuration;
 import org.openstack.atlas.service.domain.entities.Usage;
+import org.openstack.atlas.service.domain.events.UsageEvent;
 import org.openstack.atlas.service.domain.repository.UsageRepository;
 import org.w3._2005.atom.UsageEntry;
 
@@ -71,7 +73,7 @@ public class LoadBalancerAHUSLTask implements Runnable {
                     }
 
                     //Notify usage if the record was uploaded or not...
-                    if (response != null && response.getStatus() == 201) {
+                    if (response != null && response.getStatus() == HttpStatus.SC_CREATED) {
                         usageRecord.setNeedsPushed(false);
                         response.close();
                     } else if (response != null) {
@@ -82,14 +84,16 @@ public class LoadBalancerAHUSLTask implements Runnable {
                         LOG.info(String.format("body %s\n", body));
 
                         response.close();
-                        LOG.debug("\n " +
-                                "FAILED ENTRY: \nACCOUNT: " + usageRecord.getAccountId() + " LBID: " + usageRecord.getLoadbalancer().getId()
+                        LOG.debug("\n FAILED ENTRY: \n ACCOUNT: " + usageRecord.getAccountId() + " LBID: " + usageRecord.getLoadbalancer().getId()
                                 + " \nENTRY: " + entrystring + " :END FAILED ENTRY");
 
-                        usageRecord.setNeedsPushed(true);
+                        if (usageRecord.getEventType() != null && usageRecord.getEventType().equals(UsageEvent.DELETE_LOADBALANCER.name())) {
+                            usageRecord.setNeedsPushed(false);
+                        } else {
+                            usageRecord.setNeedsPushed(true);
+                        }
                     } else {
                         LOG.error("The connection timed out, updating record for re-push for load balancer: " + usageRecord.getLoadbalancer().getId());
-
                         usageRecord.setNeedsPushed(true);
                     }
                 }
