9f40e692c8c179c5b74dff0c45559a21b010c5ed
Trevor <trevor.vardeman@rackspace.com>
2013-04-08 16:26:18 -0500

    Working on the threading around pulling all snmp host information, and added a test (that is currently being ignored) to check its return status

--- jobs/src/main/java/org/openstack/atlas/usagerefactor/HostThread.java
+++ jobs/src/main/java/org/openstack/atlas/usagerefactor/HostThread.java
@@ -1,15 +1,27 @@
 package org.openstack.atlas.usagerefactor;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openstack.atlas.service.domain.entities.Host;
 
 public class HostThread implements Runnable {
+    final Log LOG = LogFactory.getLog(HostThread.class);
+    public final StingrayUsageClient stingrayUsageClient;
     public final Host host;
 
     public HostThread(Host host) {
+        stingrayUsageClient = new StingrayUsageClientImpl();
         this.host = host;
     }
 
     @Override
     public void run() {
+        try {
+            stingrayUsageClient.getHostUsage(host);
+            System.out.println("Thread has run.");
+        } catch (Exception e) {
+            String retString = String.format("Request for host %s usage from SNMP server failed.", host.getName());
+            LOG.error(retString, e);
+        }
     }
 }
