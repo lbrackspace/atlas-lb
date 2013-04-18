a4715e923bed30fe7f2173d81ee39524977bb9a7
Trevor Vardeman <trevor.vardeman@rackspace.com>
2013-04-18 14:00:53 -0500

    Working on the repository methods

--- persistence/src/main/java/org/openstack/atlas/service/domain/usage/repository/HostUsageRefactorRepository.java
+++ persistence/src/main/java/org/openstack/atlas/service/domain/usage/repository/HostUsageRefactorRepository.java
@@ -22,11 +22,14 @@ public class HostUsageRefactorRepository {
     private EntityManager entityManager;
 
     public void create(LoadBalancerHostUsage usageRecord) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO lb_usage_event (account_id, loadbalancer_id, host_id, bandwidth_out," +
+                "bandwidth_in, bandwidth_out_ssl, bandwidth_in_ssl, concurrent_connections," +
+                "concurrent_connections_ssl, tags_bitmask, num_vips, poll_time) VALUES");
         String queryString = generateFormattedValues(usageRecord);
-//        entityManager.persist(usageEventRecord);
+        entityManager.createNativeQuery(queryString);
     }
 
-    // TODO:  Logic needs tested in the requests
     public void batchCreate(List<LoadBalancerHostUsage> usageRecords) {
         LOG.info(String.format("batchCreate() called with %d records", usageRecords.size()));
         String query = generateBatchInsertQuery(usageRecords);
@@ -35,7 +38,9 @@ public class HostUsageRefactorRepository {
 
     private String generateBatchInsertQuery(List<LoadBalancerHostUsage> usages) {
         final StringBuilder sb = new StringBuilder();
-        sb.append("INSERT INTO lb_usage_event values");
+        sb.append("INSERT INTO lb_usage_event (account_id, loadbalancer_id, host_id, bandwidth_out," +
+                "bandwidth_in, bandwidth_out_ssl, bandwidth_in_ssl, concurrent_connections," +
+                "concurrent_connections_ssl, tags_bitmask, num_vips, poll_time) VALUES");
         sb.append(generateFormattedValuesForList(usages));
         return sb.toString();
     }
@@ -47,30 +52,34 @@ public class HostUsageRefactorRepository {
             queryString += "),";
         }
         if (queryString.endsWith(",")) {
-            queryString = queryString.substring(0, queryString.length() - 1);
+            queryString = queryString.substring(0, queryString.lastIndexOf(','));
         }
         return queryString;
     }
 
+    /**
+     * The order of the following appended values is deliberate.  Do no modify its order
+     * without modifying the order in the method generateBatchInsertQuery
+     */
     private String generateFormattedValues(LoadBalancerHostUsage usage) {
         StringBuilder sb = new StringBuilder();
         sb.append("(");
         sb.append(usage.getAccountId()).append(",");
         sb.append(usage.getLoadbalancerId()).append(",");
         sb.append(usage.getHostId()).append(",");
-        sb.append(usage.getIncomingTransfer()).append(",");
-        sb.append(usage.getIncomingTransferSsl()).append(",");
         sb.append(usage.getOutgoingTransfer()).append(",");
+        sb.append(usage.getIncomingTransfer()).append(",");
         sb.append(usage.getOutgoingTransferSsl()).append(",");
+        sb.append(usage.getIncomingTransferSsl()).append(",");
         sb.append(usage.getConcurrentConnections()).append(",");
         sb.append(usage.getConcurrentConnectionsSsl()).append(",");
         sb.append(usage.getTagsBitmask()).append(",");
+        sb.append(usage.getNumVips()).append(",");
 
         DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String startTime = formatter.format(usage.getPollTime().getTime());
         sb.append("'").append(startTime).append("',");
 
-        sb.append(usage.getNumVips()).append(",");
         if (usage.getEventType() == null) {
             sb.append(usage.getEventType());
         } else {
