21742f1e45e564281eab709dce754e97fe51e0d9
Trevor <trevor.vardeman@rackspace.com>
2013-04-25 09:08:57 -0500

    Adding service layer unit tests and mocking repository layer requests

---
+++ persistence/src/test/java/org/openstack/atlas/service/domain/services/UsageRefactorServiceImplTest.java
@@ -0,0 +1,50 @@
+package org.openstack.atlas.service.domain.services;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.openstack.atlas.service.domain.services.impl.UsageRefactorServiceImpl;
+import org.openstack.atlas.service.domain.usage.entities.LoadBalancerHostUsage;
+import org.openstack.atlas.service.domain.usage.entities.LoadBalancerMergedHostUsage;
+import org.openstack.atlas.service.domain.usage.repository.HostUsageRefactorRepository;
+import org.openstack.atlas.service.domain.usage.repository.LoadBalancerMergedHostUsageRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(Enclosed.class)
+public class UsageRefactorServiceImplTest {
+    public static class NodeOperations {
+        HostUsageRefactorRepository hostUsageRefactorRepository;
+        LoadBalancerMergedHostUsageRepository loadBalancerMergedHostUsageRepository;
+        UsageRefactorService usageRefactorService;
+
+        List<LoadBalancerHostUsage> hostUsageList;
+        List<LoadBalancerMergedHostUsage> mergedUsageList;
+
+        @Before
+        public void standUp() {
+            hostUsageRefactorRepository = mock(HostUsageRefactorRepository.class);
+            loadBalancerMergedHostUsageRepository = mock(LoadBalancerMergedHostUsageRepository.class);
+            usageRefactorService = new UsageRefactorServiceImpl();
+        }
+
+        @Before
+        public void buildMockResponses() {
+            hostUsageList = new ArrayList<LoadBalancerHostUsage>();
+            mergedUsageList = new ArrayList<LoadBalancerMergedHostUsage>();
+            when(hostUsageRefactorRepository.getAllLoadBalancerHostUsageRecords()).thenReturn(hostUsageList);
+            when(loadBalancerMergedHostUsageRepository.getAllUsageRecordsInOrder()).thenReturn(mergedUsageList);
+        }
+
+        @Test
+        public void placeHolder() {
+            Assert.assertFalse(false);
+        }
+    }
+}
