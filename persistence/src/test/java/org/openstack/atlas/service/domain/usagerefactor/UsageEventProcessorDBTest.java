c0abff5ec8c87bae79d5cee39ea91e08f13203e9
Phillip Toohill <phillip.toohill@rackspace.com>
2013-05-02 09:48:57 -0500

    Updating tests for event processor db test

--- persistence/src/test/java/org/openstack/atlas/service/domain/usagerefactor/UsageEventProcessorDBTest.java
+++ persistence/src/test/java/org/openstack/atlas/service/domain/usagerefactor/UsageEventProcessorDBTest.java
@@ -7,6 +7,9 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
 import org.openstack.atlas.service.domain.entities.LoadBalancer;
+import org.openstack.atlas.service.domain.entities.LoadBalancerJoinVip;
+import org.openstack.atlas.service.domain.entities.VirtualIp;
+import org.openstack.atlas.service.domain.entities.VirtualIpType;
 import org.openstack.atlas.service.domain.events.UsageEvent;
 import org.openstack.atlas.service.domain.repository.LoadBalancerRepository;
 import org.openstack.atlas.service.domain.services.UsageRefactorService;
@@ -19,10 +22,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -31,9 +31,7 @@ import static org.powermock.api.mockito.PowerMockito.when;
 @RunWith(Enclosed.class)
 public class UsageEventProcessorDBTest {
 
-//    @RunWith(MockitoJUnitRunner.class)
     @RunWith(SpringJUnit4ClassRunner.class)
-
     @ContextConfiguration(locations = {"classpath:context.xml"})
     @Transactional
     public static class WhenTestingProcessRecordsNoEvents {
@@ -75,7 +73,7 @@ public class UsageEventProcessorDBTest {
         }
 
         @Test
-        public void shouldHaveNoPreviousUsages() {
+        public void shouldHaveNoPreviousUsagesForTestDB() {
             Calendar starttime = Calendar.getInstance();
             starttime.roll(Calendar.MONTH, false);
 
@@ -89,13 +87,62 @@ public class UsageEventProcessorDBTest {
             Calendar starttime = Calendar.getInstance();
             starttime.roll(Calendar.MONTH, false);
 
-            when(loadBalancerRepository.isServicenetLoadBalancer(Matchers.<Integer>any())).thenReturn(true);
-
-            usageEventProcessor.processUsageEvent(snmpUsages, lb, UsageEvent.SSL_MIXED_ON);
+            usageEventProcessor.processUsageEvent(snmpUsages, lb, UsageEvent.SSL_ON);
             Map<Integer, Map<Integer, List<LoadBalancerHostUsage>>> oUsages = usageRefactorService.getAllLoadBalancerHostUsages();
             Assert.assertNotNull(oUsages);
             Assert.assertEquals(1, oUsages.size());
             Assert.assertEquals(true, oUsages.containsKey(543221));
+
+            Map<Integer, List<LoadBalancerHostUsage>> usagemap = oUsages.get(543221);
+            List<LoadBalancerHostUsage> lbusages = usagemap.get(1);
+
+            Assert.assertEquals(543221, lbusages.get(0).getLoadbalancerId());
+            Assert.assertEquals(55555, lbusages.get(0).getAccountId());
+            Assert.assertEquals(1, lbusages.get(0).getHostId());
+            Assert.assertEquals(1234455, lbusages.get(0).getIncomingTransfer());
+            Assert.assertEquals(4321, lbusages.get(0).getIncomingTransferSsl());
+            Assert.assertEquals(987, lbusages.get(0).getOutgoingTransfer());
+            Assert.assertEquals(986, lbusages.get(0).getOutgoingTransferSsl());
+            Assert.assertEquals(1, lbusages.get(0).getConcurrentConnections());
+            Assert.assertEquals(3, lbusages.get(0).getConcurrentConnectionsSsl());
+            Assert.assertEquals(0, lbusages.get(0).getTagsBitmask());
+        }
+
+        @Test
+        public void shouldCreateSimpleServicenetUsageRecord() {
+            Calendar starttime = Calendar.getInstance();
+            starttime.roll(Calendar.MONTH, false);
+
+            LoadBalancerJoinVip jv = new LoadBalancerJoinVip();
+            Set<LoadBalancerJoinVip> jvs = new HashSet<LoadBalancerJoinVip>();
+            VirtualIp vip = new VirtualIp();
+            vip.setVipType(VirtualIpType.SERVICENET);
+            jv.setVirtualIp(vip);
+            jvs.add(jv);
+            lb.setLoadBalancerJoinVipSet(jvs);
+
+            usageEventProcessor.setLoadBalancerRepository(loadBalancerRepository);
+            when(loadBalancerRepository.isServicenetLoadBalancer(Matchers.anyInt())).thenReturn(true);
+
+            usageEventProcessor.processUsageEvent(snmpUsages, lb, UsageEvent.SSL_OFF);
+            Map<Integer, Map<Integer, List<LoadBalancerHostUsage>>> oUsages = usageRefactorService.getAllLoadBalancerHostUsages();
+            Assert.assertNotNull(oUsages);
+            Assert.assertEquals(1, oUsages.size());
+            Assert.assertEquals(true, oUsages.containsKey(543221));
+
+            Map<Integer, List<LoadBalancerHostUsage>> usagemap = oUsages.get(543221);
+            List<LoadBalancerHostUsage> lbusages = usagemap.get(1);
+
+            Assert.assertEquals(543221, lbusages.get(0).getLoadbalancerId());
+            Assert.assertEquals(55555, lbusages.get(0).getAccountId());
+            Assert.assertEquals(1, lbusages.get(0).getHostId());
+            Assert.assertEquals(1234455, lbusages.get(0).getIncomingTransfer());
+            Assert.assertEquals(4321, lbusages.get(0).getIncomingTransferSsl());
+            Assert.assertEquals(987, lbusages.get(0).getOutgoingTransfer());
+            Assert.assertEquals(986, lbusages.get(0).getOutgoingTransferSsl());
+            Assert.assertEquals(1, lbusages.get(0).getConcurrentConnections());
+            Assert.assertEquals(3, lbusages.get(0).getConcurrentConnectionsSsl());
+            Assert.assertEquals(2, lbusages.get(0).getTagsBitmask());
         }
     }
 }
