d5de0b9520da7914f71c1b0ddeb8b17a8e6e6f06
Brandon Logan <brandon.logan@rackspace.com>
2013-06-13 11:38:51 -0500

    Added fix for race condition bug that could possibly cause overbilling.

--- jobs/src/main/java/org/openstack/atlas/usage/jobs/LoadBalancerUsagePoller.java
+++ jobs/src/main/java/org/openstack/atlas/usage/jobs/LoadBalancerUsagePoller.java
@@ -11,6 +11,7 @@ import org.openstack.atlas.service.domain.entities.JobName;
 import org.openstack.atlas.service.domain.services.HostService;
 import org.openstack.atlas.service.domain.services.UsageRefactorService;
 import org.openstack.atlas.service.domain.usage.entities.LoadBalancerHostUsage;
+import org.openstack.atlas.service.domain.usage.entities.LoadBalancerMergedHostUsage;
 import org.openstack.atlas.usagerefactor.HostThread;
 import org.openstack.atlas.usagerefactor.SnmpUsage;
 import org.openstack.atlas.usagerefactor.UsageProcessor;
@@ -65,15 +66,17 @@ public class LoadBalancerUsagePoller extends AbstractJob {
             LOG.error("There was an error retrieving current usage from stingray using snmp. " + e);
             return;
         }
-
         LOG.info("Retrieved records for " + currentUsages.size() + " hosts from stingray by SNMP.");
         UsageProcessorResult result = usageProcessor.mergeRecords(existingUsages, currentUsages, pollTime);
         LOG.info("Completed processing of current usage");
+        LOG.info("Checking if any events were inserted between the beginning of this job and now...");
+        Map<Integer, Map<Integer, List<LoadBalancerHostUsage>>> newEvents = usageRefactorService.getRecordsAfterTime(pollTime);
+        List<Integer> loadBalancersNotToDelete = removeLoadBalancerRecordsThatHadNewEvents(result, newEvents);
         usageRefactorService.batchCreateLoadBalancerMergedHostUsages(result.getMergedUsages());
         LOG.info("Completed insertion of " + result.getMergedUsages().size() + " new records into lb_merged_host_usage table.");
         usageRefactorService.batchCreateLoadBalancerHostUsages(result.getLbHostUsages());
         LOG.info("Completed insertion of " + result.getLbHostUsages().size() + " new records into lb_host_usage table.");
-        usageRefactorService.deleteOldLoadBalancerHostUsages(pollTime);
+        usageRefactorService.deleteOldLoadBalancerHostUsages(pollTime, loadBalancersNotToDelete);
         LOG.info("Completed deletion of records from lb_host_usage table prior to poll time: " + pollTime.getTime().toString());
     }
 
@@ -123,10 +126,28 @@ public class LoadBalancerUsagePoller extends AbstractJob {
         return accessibleHosts;
     }
 
-    private LoadBalancerEndpointConfiguration getConfigHost(Host host) throws DecryptException, MalformedURLException {
-        Cluster cluster = host.getCluster();
-        List<String> failoverHosts = hostService.getFailoverHostNames(cluster.getId());
-        return new LoadBalancerEndpointConfiguration(host, cluster.getUsername(), CryptoUtil.decrypt(cluster.getPassword()), host, failoverHosts, null);
+    private List<Integer> removeLoadBalancerRecordsThatHadNewEvents(UsageProcessorResult recordsToInsert,
+                                                           Map<Integer, Map<Integer, List<LoadBalancerHostUsage>>> newEvents) {
+        List<Integer> loadBalancersToExcludeFromDelete = new ArrayList<Integer>();
+        for (Integer loadbalancerId : newEvents.keySet()) {
+            Iterator<LoadBalancerMergedHostUsage> lbmhuIter = recordsToInsert.getMergedUsages().iterator();
+            while (lbmhuIter.hasNext()) {
+                LoadBalancerMergedHostUsage mergedUsage = lbmhuIter.next();
+                if(mergedUsage.getLoadbalancerId() == loadbalancerId) {
+                    LOG.info(String.format("Load balancer %d had event come in during poller run.  Removing records that were to be inserted.", loadbalancerId));
+                    loadBalancersToExcludeFromDelete.add(loadbalancerId);
+                    lbmhuIter.remove();
+                }
+            }
+            Iterator<LoadBalancerHostUsage> lbhuIter = recordsToInsert.getLbHostUsages().iterator();
+            while (lbhuIter.hasNext()) {
+                LoadBalancerHostUsage hostUsage = lbhuIter.next();
+                if (hostUsage.getLoadbalancerId() == loadbalancerId) {
+                    lbhuIter.remove();
+                }
+            }
+        }
+        return loadBalancersToExcludeFromDelete;
     }
 
     private void shutdownAndAwaitTermination(ExecutorService pool) {
