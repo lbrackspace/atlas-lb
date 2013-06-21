e4d26a21967846199f46a92ddbbad13c8c8bdb35
mich6365 <michael.gregory@rackspace.com>
2013-06-21 15:17:18 -0500

    Added ResourceTranslator Test Class

---
+++ adapter/zxtm-service/src/test/java/org/openstack/atlas/adapter/helpers/ResourceTranslatorTest.java
@@ -0,0 +1,41 @@
+package org.openstack.atlas.adapter.helpers;
+
+
+import org.junit.Before;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.openstack.atlas.docs.loadbalancers.api.management.v1.LoadBalancer;
+import org.rackspace.stingray.client.persistence.Persistence;
+import org.rackspace.stingray.client.persistence.PersistenceBasic;
+import org.rackspace.stingray.client.persistence.PersistenceProperties;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(Enclosed.class)
+public class ResourceTranslatorTest {
+
+
+
+    @RunWith(MockitoJUnitRunner.class)
+        public static class WhenTranslatingAPersistence {
+            private Persistence persistence;
+            private PersistenceProperties properties;
+            private PersistenceBasic basic;
+            private LoadBalancer mockedLoadBalancer;
+
+        @Before
+        public void standUp()
+        {
+           persistence = new Persistence();
+           properties = new PersistenceProperties();
+           basic = new PersistenceBasic();
+           mockedLoadBalancer = mock(LoadBalancer.class);
+
+
+
+
+        }
+    }
+
+}
