e9d08673b5bdb3072fb6ec1a25139cd5cf52cc03
mich6365 <michael.gregory@rackspace.com>
2013-06-24 14:53:28 -0500

    Added VS Translator Unit Test

--- adapter/zxtm-service/src/test/java/org/openstack/atlas/adapter/helpers/ResourceTranslatorTest.java
+++ adapter/zxtm-service/src/test/java/org/openstack/atlas/adapter/helpers/ResourceTranslatorTest.java
@@ -1,37 +1,129 @@
 package org.openstack.atlas.adapter.helpers;
 
-import org.junit.Before;
+
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.openstack.atlas.adapter.LoadBalancerEndpointConfiguration;
+import org.openstack.atlas.adapter.exceptions.InsufficientRequestException;
+import org.openstack.atlas.adapter.stm.STMTestBase;
+import org.openstack.atlas.adapter.stm.StmAdapterImpl;
+import org.openstack.atlas.adapter.stm.util.STMConfiguration;
 import org.openstack.atlas.docs.loadbalancers.api.management.v1.LoadBalancer;
-import org.rackspace.stingray.client.persistence.Persistence;
-import org.rackspace.stingray.client.persistence.PersistenceBasic;
-import org.rackspace.stingray.client.persistence.PersistenceProperties;
+import org.openstack.atlas.service.domain.entities.AccessList;
+import org.openstack.atlas.service.domain.entities.ConnectionLimit;
+import org.openstack.atlas.service.domain.entities.LoadBalancerProtocol;
+import org.openstack.atlas.service.domain.entities.UserPages;
+import org.rackspace.stingray.client.virtualserver.*;
 
-import static org.mockito.Mockito.mock;
+import javax.jws.soap.SOAPBinding;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-@RunWith(Enclosed.class)
-public class ResourceTranslatorTest {
 
-    @RunWith(MockitoJUnitRunner.class)
-    public static class WhenTranslatingAPersistence {
-        private Persistence persistence;
-        private PersistenceProperties properties;
-        private PersistenceBasic basic;
-        private LoadBalancer mockedLoadBalancer;
+public class ResourceTranslatorTest extends STMTestBase{
 
-        @Before
-        public void standUp() {
-            persistence = new Persistence();
-            properties = new PersistenceProperties();
-            basic = new PersistenceBasic();
-            mockedLoadBalancer = mock(LoadBalancer.class);
+
+
+
+    public static class WhenTranslatingAVirtualServer {
+
+
+        String vsName;
+        Boolean isHalfClosed;
+        Boolean isConnectionLogging;
+        Boolean isContentCaching;
+        String logFormat;
+        VirtualServerTcp expectedTcp;
+        VirtualServerConnectionError expectedError;
+        List<String> rules;
+        ResourceTranslator translator;
+        String errorFile;
+
+
+        public void standUp(String logFormat, LoadBalancerProtocol protocol) {
+            setupIvars();
+            createSimpleLoadBalancer();
+            vsName = "test_name";
+            this.isConnectionLogging = true;
+            isHalfClosed = true;
+            isContentCaching = true;
+            expectedTcp = new VirtualServerTcp();
+            expectedError = new VirtualServerConnectionError();
+            errorFile = "Default";
+            expectedError.setError_file(errorFile);
+
+            rules = java.util.Arrays.asList(StmAdapterImpl.XFF, StmAdapterImpl.XFP);
+            translator = new ResourceTranslator();
+
+            ConnectionLimit connectionLimit = new ConnectionLimit();
+            Set<AccessList> accessListSet = new HashSet<AccessList>();
+            accessListSet.add(new AccessList());
+            this.logFormat = logFormat;
+
+
+            lb.setConnectionLogging(isConnectionLogging);
+            lb.setHalfClosed(isHalfClosed);
+            lb.setContentCaching(isContentCaching);
+            lb.setProtocol(protocol);
+            lb.setConnectionLimit(connectionLimit);
+            lb.setAccessLists(accessListSet);
+        }
+
+         public void standUp(String logFormat, LoadBalancerProtocol protocol, String errorFile)
+         {
+
+             standUp(logFormat, protocol);
+             this.errorFile = errorFile;
+             UserPages userPages = new UserPages();
+             userPages.setErrorpage(this.errorFile);
+             lb.setUserPages(userPages);
+             expectedError.setError_file(this.errorFile);
+
+
+         }
+
+
+        @Test
+        public void shouldCreateValidVirtualServer() throws InsufficientRequestException {
+            standUp("%v %{Host}i %h %l %u %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %n", LoadBalancerProtocol.HTTP);
+            VirtualServer createdServer = translator.translateVirtualServerResource(config, vsName, lb);
+            VirtualServerProperties createdProperties = createdServer.getProperties();
+            VirtualServerBasic createdBasic = createdServer.getProperties().getBasic();
+            VirtualServerTcp createdTcp = createdProperties.getTcp();
+            expectedTcp.setProxy_close(isHalfClosed);
+            VirtualServerLog log = createdProperties.getLog();
+            Boolean cacheEnabled = createdProperties.getWeb_cache().getEnabled();
+            Assert.assertNotNull(log);
+            Assert.assertEquals(logFormat, log.getFormat());
+            Assert.assertTrue(cacheEnabled);
+            Assert.assertEquals(lb.getProtocol().name(), createdBasic.getProtocol());
+            Assert.assertEquals(lb.getPort(), createdBasic.getPort());
+            Assert.assertEquals(vsName, createdBasic.getPool());
+            Assert.assertTrue(createdBasic.getEnabled());
+            Assert.assertEquals(vsName, createdBasic.getProtection_class());
+            Assert.assertEquals(expectedTcp, createdTcp);
+            Assert.assertFalse(createdBasic.getListen_on_any());
+            Assert.assertEquals(rules, createdBasic.getRequest_rules());
+            Assert.assertEquals(expectedError, createdProperties.getConnection_errors());
         }
 
         @Test
-        public void testStub() {
+        public void checkAlternateValidPaths() throws InsufficientRequestException
+        {
+            standUp("%v %t %h %A:%p %n %B %b %T", LoadBalancerProtocol.FTP, "HI");
+            VirtualServer createdServer = translator.translateVirtualServerResource(config, vsName, lb);
+            VirtualServerProperties createdProperties = createdServer.getProperties();
+            VirtualServerBasic createdBasic = createdServer.getProperties().getBasic();
+            VirtualServerLog log = createdProperties.getLog();
+            Assert.assertEquals(logFormat, log.getFormat());
+            Assert.assertEquals(expectedError, createdProperties.getConnection_errors());
+
+
+
+
         }
+
+
     }
 }
