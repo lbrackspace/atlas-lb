641cd8e87c2f4c428dfe130706dbb4e82484564b
mich6365 <michael.gregory@rackspace.com>
2013-07-16 11:20:07 -0500

    Added file for STMAdapter Unit Tests

---
+++ adapter/zxtm-service/src/test/java/org/openstack/atlas/adapter/stm/StmAdapterImplTest.java
@@ -0,0 +1,49 @@
+package org.openstack.atlas.adapter.stm;
+
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+@RunWith(Enclosed.class)
+public class StmAdapterImplTest {
+
+    public static class LoadBalancerOperationsTest {
+
+    }
+
+    public static class VirtualServerOperationsTest {
+
+    }
+
+    public static class PoolOperationsTest {
+
+    }
+
+    public static class VirtualIpOperationsTest {
+
+    }
+
+    public static class HealthMonitorOperationsTest {
+
+    }
+
+    public static class ProtectionOperationsTest {
+
+    }
+
+    public static class SslTerminationOperationsTest {
+
+    }
+
+    public static class RateLimitOperationsTest {
+
+    }
+
+    public  static class ErrorFileOperationsTest {
+
+    }
+
+    public static class SubnetMappingOperationsTest {
+
+    }
+
+}
