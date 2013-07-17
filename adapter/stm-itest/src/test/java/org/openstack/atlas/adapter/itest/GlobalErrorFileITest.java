3a5409eb9677b85e9b588051c34652888eb604d9
Adam Harwell <flux.adam@gmail.com>
2013-07-17 14:54:46 -0500

    Fixing GlobalErrorFileITest operations.

--- adapter/stm-itest/src/test/java/org/openstack/atlas/adapter/itest/GlobalErrorFileITest.java
+++ adapter/stm-itest/src/test/java/org/openstack/atlas/adapter/itest/GlobalErrorFileITest.java
@@ -8,6 +8,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openstack.atlas.adapter.exceptions.InsufficientRequestException;
 import org.openstack.atlas.adapter.exceptions.RollBackException;
+import org.openstack.atlas.adapter.exceptions.StmRollBackException;
 import org.openstack.atlas.adapter.helpers.ZxtmNameBuilder;
 import org.openstack.atlas.service.domain.util.Constants;
 import org.rackspace.stingray.client.exception.StingrayRestClientException;
@@ -19,7 +20,9 @@ import java.io.FileReader;
 import java.rmi.RemoteException;
 
 public class GlobalErrorFileITest extends STMTestBase {
-    String pageContent = "ERROR PAGE CONTENT";
+    String defaultPageContent = "DEFAULT ERROR PAGE CONTENT";
+    String customPageContent = "CUSTOM ERROR PAGE CONTENT";
+    String customPageContent2 = "CUSTOM ERROR PAGE CONTENT 2";
     String vsName;
 
 
@@ -37,32 +40,66 @@ public class GlobalErrorFileITest extends STMTestBase {
     }
 
     @Test
-    public void testSimpleErrorFileOperations() throws Exception {
+    public void testDefaultErrorFileOperations() throws Exception {
+        setDefaultErrorFile();
+    }
+
+    @Test
+    public void testCustomErrorFileOperations() throws Exception {
         setCustomErrorFile();
     }
 
     @Test(expected = StingrayRestClientObjectNotFoundException.class)
     public void testSimpleDeleteErrorFileOperations() throws Exception {
-        setCustomErrorFile();
+        //setCustomErrorFile();
         deleteErrorFile();
     }
 
-    private void setCustomErrorFile() throws Exception {
+    private void setDefaultErrorFile() throws Exception {
         vsName = ZxtmNameBuilder.genVSName(lb);
 
         //This is mgmt call to set 'default' file other than stm Default, lb should have Default at this point.
-        stmAdapter.uploadDefaultErrorFile(config, pageContent);
+        stmAdapter.uploadDefaultErrorFile(config, defaultPageContent);
         File file = stmClient.getExtraFile(Constants.DEFAULT_ERRORFILE);
         Assert.assertNotNull(file);
 
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String content = reader.readLine();
-        Assert.assertEquals(pageContent, content);
+        Assert.assertEquals(defaultPageContent, content);
+    }
+
+    private void setCustomErrorFile() throws Exception {
+        vsName = ZxtmNameBuilder.genVSName(lb);
+
+        Assert.assertFalse(customPageContent.equals(customPageContent2)); //assert our assumption
+
+        stmAdapter.setErrorFile(config, lb, customPageContent);
+        File file = stmClient.getExtraFile(errorFileName());
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String content = reader.readLine();
+        Assert.assertEquals(customPageContent, content);
+
+        stmAdapter.setErrorFile(config, lb, customPageContent2);
+        file = stmClient.getExtraFile(errorFileName());
+        reader = new BufferedReader(new FileReader(file));
+        content = reader.readLine();
+        Assert.assertEquals(customPageContent2, content);
     }
 
     //TODO: need to add delete to adapter. This is a 'global' file and only used by ops(which they have yet to ever use)
-    private void deleteErrorFile() throws RollBackException, AxisFault, InsufficientRequestException, StingrayRestClientException, StingrayRestClientObjectNotFoundException {
-//        stmAdapter.deleteErrorFile(config, lb);
-//        stmClient.getExtraFile(errorFileName());
+    private void deleteErrorFile() throws InsufficientRequestException, StmRollBackException, StingrayRestClientException, StingrayRestClientObjectNotFoundException {
+        final String errorFileName = errorFileName();
+        File errorFile = stmClient.getExtraFile(errorFileName);
+
+        Assert.assertNotNull(errorFile);
+
+        stmAdapter.deleteErrorFile(config, lb);
+        errorFile = null;
+        try {
+             errorFile = stmClient.getExtraFile(errorFileName);
+        } catch (StingrayRestClientObjectNotFoundException e) {
+            //yup
+        }
+        Assert.assertNull(errorFile);
     }
 }
