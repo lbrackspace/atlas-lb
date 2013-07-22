f75b48db47a290c0191745f1fe9d8a380f7c36a2
Adam Harwell <flux.adam@gmail.com>
2013-07-22 11:30:29 -0500

    UpdateConnectionLoggingListener unit test.

--- api/common-api/src/test/java/org/openstack/atlas/api/async/UpdateConnectionLoggingListenerTest.java
+++ api/common-api/src/test/java/org/openstack/atlas/api/async/UpdateConnectionLoggingListenerTest.java
@@ -1,11 +1,105 @@
 package org.openstack.atlas.api.async;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openstack.atlas.api.async.util.STMTestBase;
+import org.openstack.atlas.api.integration.ReverseProxyLoadBalancerStmService;
+import org.openstack.atlas.service.domain.entities.LoadBalancerStatus;
+import org.openstack.atlas.service.domain.events.entities.CategoryType;
+import org.openstack.atlas.service.domain.events.entities.EventSeverity;
+import org.openstack.atlas.service.domain.events.entities.EventType;
+import org.openstack.atlas.service.domain.exceptions.EntityNotFoundException;
+import org.openstack.atlas.service.domain.services.LoadBalancerService;
+import org.openstack.atlas.service.domain.services.NotificationService;
+import org.openstack.atlas.service.domain.services.helpers.AlertType;
 
-public class UpdateConnectionLoggingListenerTest {
+import javax.jms.ObjectMessage;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+public class UpdateConnectionLoggingListenerTest extends STMTestBase {
+    private Integer LOAD_BALANCER_ID;
+    private Integer ACCOUNT_ID;
+    private String USERNAME = "SOME_USERNAME";
+
+    @Mock
+    private ObjectMessage objectMessage;
+    @Mock
+    private LoadBalancerService loadBalancerService;
+    @Mock
+    private NotificationService notificationService;
+    @Mock
+    private ReverseProxyLoadBalancerStmService reverseProxyLoadBalancerStmService;
+
+    private UpdateConnectionLoggingListener updateConnectionLoggingListener;
+
+    @Before
+    public void standUp() {
+        MockitoAnnotations.initMocks(this);
+        setupIvars();
+        LOAD_BALANCER_ID = lb.getId();
+        ACCOUNT_ID = lb.getAccountId();
+        lb.setUserName(USERNAME);
+        updateConnectionLoggingListener = new UpdateConnectionLoggingListener();
+        updateConnectionLoggingListener.setLoadBalancerService(loadBalancerService);
+        updateConnectionLoggingListener.setNotificationService(notificationService);
+        updateConnectionLoggingListener.setReverseProxyLoadBalancerStmService(reverseProxyLoadBalancerStmService);
+    }
+
     @Test
-       public void sampleTest() {
+    public void testUpdateLoadBalancerWithLoggingTrue() throws Exception {
+        lb.setConnectionLogging(true);
+        when(objectMessage.getObject()).thenReturn(lb);
+        when(loadBalancerService.get(LOAD_BALANCER_ID, ACCOUNT_ID)).thenReturn(lb);
 
-       }
+        updateConnectionLoggingListener.doOnMessage(objectMessage);
 
+        verify(reverseProxyLoadBalancerStmService).updateLoadBalancer(lb, lb);
+        verify(notificationService).saveLoadBalancerEvent(eq(USERNAME), eq(ACCOUNT_ID), eq(LOAD_BALANCER_ID), anyString(), anyString(), eq(EventType.UPDATE_CONNECTION_LOGGING), eq(CategoryType.UPDATE), eq(EventSeverity.INFO));
+        verify(loadBalancerService).setStatus(lb, LoadBalancerStatus.ACTIVE);
+    }
+
+    @Test
+    public void testUpdateLoadBalancerWithLoggingFalse() throws Exception {
+        lb.setConnectionLogging(false);
+        when(objectMessage.getObject()).thenReturn(lb);
+        when(loadBalancerService.get(LOAD_BALANCER_ID, ACCOUNT_ID)).thenReturn(lb);
+
+        updateConnectionLoggingListener.doOnMessage(objectMessage);
+
+        verify(reverseProxyLoadBalancerStmService).updateLoadBalancer(lb, lb);
+        verify(notificationService).saveLoadBalancerEvent(eq(USERNAME), eq(ACCOUNT_ID), eq(LOAD_BALANCER_ID), anyString(), anyString(), eq(EventType.UPDATE_CONNECTION_LOGGING), eq(CategoryType.UPDATE), eq(EventSeverity.INFO));
+        verify(loadBalancerService).setStatus(lb, LoadBalancerStatus.ACTIVE);
+    }
+
+    @Test
+    public void testUpdateInvalidLoadBalancer() throws Exception {
+        EntityNotFoundException entityNotFoundException = new EntityNotFoundException();
+        when(objectMessage.getObject()).thenReturn(lb);
+        when(loadBalancerService.get(LOAD_BALANCER_ID, ACCOUNT_ID)).thenThrow(entityNotFoundException);
+
+        updateConnectionLoggingListener.doOnMessage(objectMessage);
+
+        verify(notificationService).saveAlert(eq(ACCOUNT_ID), eq(LOAD_BALANCER_ID), eq(entityNotFoundException), eq(AlertType.DATABASE_FAILURE.name()), anyString());
+        verify(notificationService).saveLoadBalancerEvent(eq(USERNAME), eq(ACCOUNT_ID), eq(LOAD_BALANCER_ID), anyString(), anyString(), eq(EventType.UPDATE_CONNECTION_LOGGING), eq(CategoryType.UPDATE), eq(EventSeverity.CRITICAL));
+    }
+
+    @Test
+    public void testUpdateLoadBalancerWithInvalidLogging() throws Exception {
+        Exception exception = new Exception();
+        when(objectMessage.getObject()).thenReturn(lb);
+        when(loadBalancerService.get(LOAD_BALANCER_ID, ACCOUNT_ID)).thenReturn(lb);
+        doThrow(exception).when(reverseProxyLoadBalancerStmService).updateLoadBalancer(lb, lb);
+
+        updateConnectionLoggingListener.doOnMessage(objectMessage);
+
+        verify(reverseProxyLoadBalancerStmService).updateLoadBalancer(lb, lb);
+        verify(loadBalancerService).setStatus(lb, LoadBalancerStatus.ERROR);
+        verify(notificationService).saveAlert(eq(ACCOUNT_ID), eq(LOAD_BALANCER_ID), eq(exception), eq(AlertType.ZEUS_FAILURE.name()), anyString());
+        verify(notificationService).saveLoadBalancerEvent(eq(USERNAME), eq(ACCOUNT_ID), eq(LOAD_BALANCER_ID), anyString(), anyString(), eq(EventType.UPDATE_CONNECTION_LOGGING), eq(CategoryType.UPDATE), eq(EventSeverity.CRITICAL));
+    }
 }
