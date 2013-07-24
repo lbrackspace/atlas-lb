4d88ec64ae729973f27815835a657576ae9a4c5d
Phillip Toohill <phillip.toohill@rackspace.com>
2013-07-24 16:10:56 -0500

    working on full config i tests

--- adapter/stm-itest/src/test/java/org/openstack/atlas/adapter/itest/FullConfigIntegrationTest.java
+++ adapter/stm-itest/src/test/java/org/openstack/atlas/adapter/itest/FullConfigIntegrationTest.java
@@ -7,14 +7,18 @@ import org.junit.Test;
 import org.openstack.atlas.adapter.exceptions.InsufficientRequestException;
 import org.openstack.atlas.adapter.helpers.ResourceTranslator;
 import org.openstack.atlas.adapter.helpers.StmConstants;
+import org.openstack.atlas.adapter.helpers.ZeusNodePriorityContainer;
 import org.openstack.atlas.service.domain.entities.*;
 import org.rackspace.stingray.client.StingrayRestClient;
 import org.rackspace.stingray.client.exception.StingrayRestClientException;
 import org.rackspace.stingray.client.exception.StingrayRestClientObjectNotFoundException;
 import org.rackspace.stingray.client.pool.Pool;
+import org.rackspace.stingray.client.pool.PoolNodeWeight;
 import org.rackspace.stingray.client.virtualserver.VirtualServer;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.openstack.atlas.service.domain.entities.AccessListType.ALLOW;
@@ -22,6 +26,9 @@ import static org.openstack.atlas.service.domain.entities.AccessListType.DENY;
 import static org.openstack.atlas.service.domain.entities.SessionPersistence.HTTP_COOKIE;
 
 public class FullConfigIntegrationTest extends STMTestBase {
+    Node n;
+    Node n2;
+    Node n3;
 
     @BeforeClass
     public static void setupClass() throws InterruptedException {
@@ -37,9 +44,26 @@ public class FullConfigIntegrationTest extends STMTestBase {
 
     @Test
     public void createFullyConfiguredLoadBalancer() {
+        try {
+            removeLoadBalancer();
+            stmAdapter.createLoadBalancer(config, buildHydratedLb());
+            Thread.sleep(1000);
+            StingrayRestClient tclient = new StingrayRestClient();
+            verifyVS(tclient);
+            verifyPool(tclient);
+            removeLoadBalancer();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    private LoadBalancer buildHydratedLb() {
         lb.setAlgorithm(LoadBalancerAlgorithm.WEIGHTED_ROUND_ROBIN);
         lb.setSessionPersistence(HTTP_COOKIE);
         lb.setTimeout(99);
+        lb.setConnectionLogging(true);
+        lb.setSessionPersistence(SessionPersistence.HTTP_COOKIE);
 
         HealthMonitor monitor = new HealthMonitor();
         monitor.setType(HealthMonitorType.CONNECT);
@@ -53,9 +77,7 @@ public class FullConfigIntegrationTest extends STMTestBase {
         limit.setRateInterval(10);
         limit.setMaxConnectionRate(10);
         limit.setMinConnections(1);
-//        lb.setConnectionLimit(limit);
-
-        lb.setConnectionLogging(true);
+        lb.setConnectionLimit(limit);
 
         Set<AccessList> networkItems = new HashSet<AccessList>();
         AccessList item1 = new AccessList();
@@ -67,20 +89,37 @@ public class FullConfigIntegrationTest extends STMTestBase {
         networkItems.add(item1);
         networkItems.add(item2);
 
-//        lb.setAccessLists(networkItems);
+        lb.setAccessLists(networkItems);
 
-        try {
-            removeLoadBalancer();
-            stmAdapter.createLoadBalancer(config, lb);
-            Thread.sleep(1000);
-            StingrayRestClient tclient = new StingrayRestClient();
-            verifyVS(tclient);
-            verifyPool(tclient);
-            removeLoadBalancer();
-        } catch (Exception e) {
-            e.printStackTrace();
-            Assert.fail(e.getMessage());
-        }
+        n = new Node();
+        n.setWeight(1);
+        n.setType(NodeType.SECONDARY);
+        n.setIpAddress("10.3.3.3");
+        n.setCondition(NodeCondition.ENABLED);
+        n.setPort(8080);
+        n.setStatus(NodeStatus.ONLINE);
+
+        n2 = new Node();
+        n2.setWeight(3);
+        n2.setType(NodeType.SECONDARY);
+        n2.setIpAddress("10.3.3.5");
+        n2.setCondition(NodeCondition.DISABLED);
+        n2.setPort(7070);
+        n2.setStatus(NodeStatus.OFFLINE);
+
+        n3 = new Node();
+        n3.setWeight(3);
+        n3.setType(NodeType.SECONDARY);
+        n3.setIpAddress("10.3.3.9");
+        n3.setCondition(NodeCondition.DRAINING);
+        n3.setPort(9090);
+        n3.setStatus(NodeStatus.OFFLINE);
+
+        lb.getNodes().add(n);
+        lb.getNodes().add(n2);
+        lb.getNodes().add(n3);
+
+        return lb;
     }
 
     private VirtualServer verifyVS(StingrayRestClient client) throws InsufficientRequestException, StingrayRestClientException, StingrayRestClientObjectNotFoundException {
@@ -98,17 +137,40 @@ public class FullConfigIntegrationTest extends STMTestBase {
         Assert.assertEquals(false, vs.getProperties().getTcp().getProxy_close());
         Assert.assertEquals(vs.getProperties().getBasic().getListen_on_traffic_ips(), translator.genGroupNameSet(lb));
 
-//        Assert.assertEquals(protectionClassName(), vs.getProperties().getBasic().getProtection_class());
-        return null;
+        Assert.assertEquals(protectionClassName(), vs.getProperties().getBasic().getProtection_class());
+        return vs;
     }
 
-    private VirtualServer verifyPool(StingrayRestClient client) throws InsufficientRequestException, StingrayRestClientException, StingrayRestClientObjectNotFoundException {
+    private Pool verifyPool(StingrayRestClient client) throws InsufficientRequestException, StingrayRestClientException, StingrayRestClientObjectNotFoundException {
         Pool pool = client.getPool(loadBalancerName());
         Assert.assertNotNull(pool);
         Assert.assertEquals(1, pool.getProperties().getBasic().getMonitors().size());
         Assert.assertEquals(lb.getAlgorithm().name().toLowerCase(), pool.getProperties().getLoad_balancing().getAlgorithm());
-        Assert.assertEquals(1, pool.getProperties().getBasic().getNodes().size());
+        Assert.assertEquals(3, pool.getProperties().getBasic().getNodes().size());
 
-        return null;
+        Assert.assertEquals(2, pool.getProperties().getBasic().getDisabled().size());
+        Assert.assertEquals(1, pool.getProperties().getBasic().getDraining().size());
+
+        List<PoolNodeWeight> pws = pool.getProperties().getLoad_balancing().getNode_weighting();
+        for (PoolNodeWeight pnw : pws) {
+            Node lbn = lb.getNodes().iterator().next();
+            if (lbn.getIpAddress().equals(pnw.getNode().split(":")[0])) {
+                Assert.assertEquals(lbn.getIpAddress() + ":" + lbn.getPort(),pnw.getNode());
+                Assert.assertEquals(lbn.getWeight(), pnw.getWeight());
+            }
+        }
+
+        ZeusNodePriorityContainer znpc = new ZeusNodePriorityContainer(lb.getNodes());
+        Assert.assertEquals(znpc.getPriorityValuesSet(), pool.getProperties().getLoad_balancing().getPriority_values());
+        Assert.assertEquals(znpc.hasSecondary(), pool.getProperties().getLoad_balancing().getPriority_enabled());
+
+        Assert.assertEquals(lb.getTimeout(), pool.getProperties().getConnection().getMax_reply_time());
+
+        Assert.assertEquals(new HashSet<String>(Arrays.asList(loadBalancerName())), pool.getProperties().getBasic().getMonitors());
+
+        Assert.assertEquals(lb.getSessionPersistence().name(), pool.getProperties().getBasic().getPersistence_class());
+        Assert.assertEquals("", pool.getProperties().getBasic().getBandwidth_class());
+
+        return pool;
     }
 }
