d7e564bdd6bab1c207260885fc754c9cdc817c0c
Trevor Vardeman <trevor.vardeman@rackspace.com>
2013-09-30 13:25:23 -0500

    Fixed the node validation errors

--- api/common-api/src/main/java/org/openstack/atlas/api/validation/verifiers/DuplicateNodeVerifier.java
+++ api/common-api/src/main/java/org/openstack/atlas/api/validation/verifiers/DuplicateNodeVerifier.java
@@ -5,18 +5,19 @@ import org.openstack.atlas.api.validation.verifiers.Verifier;
 import org.openstack.atlas.api.validation.verifiers.VerifierResult;
 import org.openstack.atlas.docs.loadbalancers.api.v1.Nodes;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class DuplicateNodeVerifier implements Verifier<Nodes> {
+public class DuplicateNodeVerifier implements Verifier<List<Node>> {
 
-    public VerifierResult verify(Nodes nodes) {
+    public VerifierResult verify(List<Node> nodes) {
         Set<String> ipAddressesAndPorts = new HashSet<String>();
         if(nodes == null) {
             return new VerifierResult(true); // Don't flag the user for duplicate nodes when the nodes must not be empty verifier already flagged the user.
         }
-        for (Node node : nodes.getNodes()) {
+        for (Node node : nodes) {
             String ipAddressAndPort = node.getAddress() + ":" + node.getPort();
             if (!ipAddressesAndPorts.add(ipAddressAndPort)) {
                 return new VerifierResult(false);
