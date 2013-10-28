b401738a5dadd1fefd394ac042727aafadc8c467
Phillip Toohill <phillip.toohill@rackspace.com>
2013-10-28 16:37:47 -0500

    Updating migrations for meta-v61; AH referenceIDs are now being tracked

--- migration/schema/loadbalancing/60-61.sql
+++ migration/schema/loadbalancing/60-61.sql
@@ -2,4 +2,4 @@ use `loadbalancing`;
 
 ALTER TABLE `lb_usage` ADD `reference_id` VARCHAR(256);
 
-update `meta` set `meta_value` = '???' where `meta_key`='version';
+update `meta` set `meta_value` = '61' where `meta_key`='version';
--- migration/schema/loadbalancing/61-60.sql
+++ migration/schema/loadbalancing/61-60.sql
@@ -2,4 +2,4 @@ use `loadbalancing`;
 
 ALTER TABLE `lb_usage` DROP COLUMN `reference_id`;
 
-update `meta` set `meta_value` = '???' where `meta_key`='version';
+update `meta` set `meta_value` = '60' where `meta_key`='version';
