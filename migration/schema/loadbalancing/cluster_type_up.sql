8b4abef8abfaa7d823f0b4ddd01653cea9a97fc0
Carlos D. Garza <carlos.garza@rackspace.com>
2013-12-16 18:45:58 -0600

    Adding migration for cluster_type colunm in cluster table. :|

---
+++ migration/schema/loadbalancing/cluster_type_up.sql
@@ -0,0 +1,17 @@
+use loadbalancing;
+
+CREATE TABLE cl_type(
+    name varchar(32),
+    description varchar(128) NOT NULL,
+    primary key(name)
+) engine=InnoDB default charset=utf8;
+INSERT INTO cl_type(name,description)values('STANDARD','Normal User account');
+INSERT INTO cl_type(name,description)values('INTERNAL','Internal Rackspace account');
+
+ALTER TABLE account ADD COLUMN cluster_type varchar(32) NOT NULL DEFAULT 'STANDARD';
+ALTER TABLE cluster ADD COLUMN cluster_type varchar(32) NOT NULL DEFAULT 'STANDARD';
+
+ALTER TABLE account ADD CONSTRAINT fk_account_cl_type foreign key(cluster_type) references cl_type(name);
+ALTER TABLE cluster ADD CONSTRAINT fk_cluster_cl_type foreign key(cluster_type) references cl_type(name);
+
+update meta set `meta_value` = '???; where `meta_key`='version';
