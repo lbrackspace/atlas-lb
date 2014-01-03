c4911a65ee8c7c9c7c77e8fcfa2111ac9ec6ef5a
Carlos D. Garza <carlos.garza@rackspace.com>
2014-01-03 15:12:39 -0600

    Fixing Migration schemas for 58-59

--- migration/schema/loadbalancing/58-59.sql
+++ migration/schema/loadbalancing/58-59.sql
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-USE `loadbalancing`;
-
-ALTER TABLE `host` ADD COLUMN `rest_endpoint` VARCHAR (255) NOT NULL;
-ALTER TABLE `host` ADD COLUMN `rest_endpoint_active` tinyint(1) NOT NULL;
-UPDATE `host` SET `rest_endpoint_active`='1';
-ALTER TABLE `host` modify COLUMN `rest_endpoint_active` tinyint(1) NOT NULL;
-INSERT INTO host_status(NAME,description)VALUES("REST_API_ENDPOINT","Indicates that this Host shall handle all REST requests for this cluster.");
-=======
 use `loadbalancing`;
 
 INSERT INTO lb_data_center values('HKG','Hong Kong');
->>>>>>> 1.19-candidate
 
 UPDATE `meta` SET `meta_value` = '59' WHERE `meta_key`='version';
---
+++ migration/schema/loadbalancing/rest_endpoint_down.sql
@@ -0,0 +1,9 @@
+USE `loadbalancing`;
+
+ALTER TABLE `host` DROP COLUMN `rest_endpoint`;
+ALTER TABLE `host` DROP COLUMN `rest_endpoint_active`;
+delete from host_status where name="REST_API_ENDPOINT";
+
+
+update `meta` set `meta_value` = '???' where `meta_key`='version';
+
