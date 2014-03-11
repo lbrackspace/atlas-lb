0114ebc9a2d29675da7d1bd92be126324c91d40e
Carlos D. Garza <carlos.garza@rackspace.com>
2014-03-11 12:57:48 -0500

    Mangle up jython tests for SSL experiments.

--- itest/jython-tests/test.py
+++ itest/jython-tests/test.py
@@ -10,14 +10,14 @@ setConfFile("./dev.json")
 
 app = getDb()
 #db.buildClassImportFile("db_classes.py",utils.conf_file)
+zt = getZeusTest(hid=1)
 
 from db_classes import *
 
-a = Account()
-a.setId(1)
-sha1=HashUtil.sha1sumHex("1",0,4)
-a.setSha1SumForIpv6(sha1)
-a.setClusterType(ClusterType.INTERNAL)
+#a = Account()
+#a.setId(1)
+#sha1=HashUtil.sha1sumHex("1",0,4)
+#a.setSha1SumForIpv6(sha1)
+#a.setClusterType(ClusterType.INTERNAL)
 
 
-zt = getZeusTest(hid=1)
