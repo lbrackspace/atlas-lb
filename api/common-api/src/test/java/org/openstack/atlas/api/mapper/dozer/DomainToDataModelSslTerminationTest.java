b02a1c4bb6a2b9efe79cd2a51ef07611dc39834e
Carlos D. Garza <carlos.garza@rackspace.com>
2014-03-24 15:27:41 -0500

    Got dozermap tests to work with SslTermination objects.

--- api/common-api/src/test/java/org/openstack/atlas/api/mapper/dozer/DomainToDataModelSslTerminationTest.java
+++ api/common-api/src/test/java/org/openstack/atlas/api/mapper/dozer/DomainToDataModelSslTerminationTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.openstack.atlas.service.domain.entities.SslTermination;
+import org.openstack.atlas.util.debug.Debug;
 
 @RunWith(Enclosed.class)
 public class DomainToDataModelSslTerminationTest {
@@ -16,8 +17,8 @@ public class DomainToDataModelSslTerminationTest {
     public static class When_mapping_ssl_termination_from_domain_to_datamodel {
 
         private DozerBeanMapper mapper;
-        private SslTermination sslTermination;
-        private org.openstack.atlas.docs.loadbalancers.api.v1.SslTermination domainSsltermination;
+        private SslTermination domainSslTermination;
+        private org.openstack.atlas.docs.loadbalancers.api.v1.SslTermination apiSsltermination;
 
         @Before
         public void standUp() {
@@ -26,23 +27,30 @@ public class DomainToDataModelSslTerminationTest {
 
         @Test
         public void shouldMapToNullWhenSessionPersistenceTypeIsNONE() {
-            sslTermination = new SslTermination();
-            sslTermination.setPrivatekey("AKey");
-            sslTermination.setCertificate("aCert");
-            sslTermination.setIntermediateCertificate("anIntermediateCert");
-            sslTermination.setSecurePort(443);
+            domainSslTermination = new SslTermination();
+            domainSslTermination.setPrivatekey("AKey");
+            domainSslTermination.setCertificate("aCert");
+            domainSslTermination.setReEncryptionCertificateAuthority("aCA");
+            domainSslTermination.setReEncryptionEnabled(Boolean.TRUE);
+            domainSslTermination.setIntermediateCertificate("anIntermediateCert");
+            domainSslTermination.setSecurePort(443);
             try {
-                domainSsltermination = mapper.map(sslTermination, org.openstack.atlas.docs.loadbalancers.api.v1.SslTermination.class);
+                apiSsltermination = mapper.map(domainSslTermination, org.openstack.atlas.docs.loadbalancers.api.v1.SslTermination.class);
             } catch (Exception e) {
-                Assert.fail("Exception caused by ssl termination types being null");
+                String exMsg = Debug.getExtendedStackTrace(e);
+                Assert.fail(String.format("someException caused by converting domain to api SslTermination %s", exMsg));
             }
 
-            Assert.assertEquals(sslTermination.getPrivatekey(), domainSsltermination.getPrivatekey());
-            Assert.assertEquals(sslTermination.getCertificate(), domainSsltermination.getCertificate());
-            Assert.assertEquals(sslTermination.getIntermediateCertificate(), domainSsltermination.getIntermediateCertificate());
-            Assert.assertEquals(sslTermination.getSecurePort(), (long)domainSsltermination.getSecurePort());
-            Assert.assertEquals(true, domainSsltermination.isEnabled());
-            Assert.assertEquals(false, domainSsltermination.isSecureTrafficOnly());
+            Assert.assertEquals(domainSslTermination.getPrivatekey(), apiSsltermination.getPrivatekey());
+            Assert.assertEquals(domainSslTermination.getCertificate(), apiSsltermination.getCertificate());
+            Assert.assertEquals(domainSslTermination.getIntermediateCertificate(), apiSsltermination.getIntermediateCertificate());
+            Assert.assertEquals(domainSslTermination.getSecurePort(), (long) apiSsltermination.getSecurePort());
+            Assert.assertEquals(true, apiSsltermination.isEnabled());
+            Assert.assertEquals(false, apiSsltermination.isSecureTrafficOnly());
+            Assert.assertEquals(true, apiSsltermination.isReEncryptionEnabled());
+            Assert.assertEquals(domainSslTermination.isReEncryptionEnabled(), apiSsltermination.isReEncryptionEnabled());
+            Assert.assertEquals("aCA", apiSsltermination.getReEncryptionCertificateAuthority());
+            Assert.assertEquals(domainSslTermination.getReEncryptionCertificateAuthority(), apiSsltermination.getReEncryptionCertificateAuthority());
         }
     }
 }
