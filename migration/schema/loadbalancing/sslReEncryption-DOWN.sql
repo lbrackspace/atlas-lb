78f9e9ea31e4261f4f8b2f013aabd7ec2844fb10
Carlos D. Garza <carlos.garza@rackspace.com>
2014-03-24 12:23:26 -0500

    Readding up and down CA Certificate Migration script.

--- migration/schema/loadbalancing/sslReEncryption-DOWN.sql
+++ migration/schema/loadbalancing/sslReEncryption-DOWN.sql
@@ -3,4 +3,4 @@ use loadbalancing;
 ALTER TABLE lb_ssl DROP COLUMN reencryption;
 ALTER TABLE lb_ssl DROP COLUMN certificate_authority;
 
-update meta set `meta_value` = '63' where `meta_key`='version';
+update meta set `meta_value` = '??' where `meta_key`='version';
--- migration/schema/loadbalancing/sslReEncryption-UP.sql
+++ migration/schema/loadbalancing/sslReEncryption-UP.sql
@@ -3,4 +3,4 @@ use loadbalancing;
 ALTER TABLE lb_ssl ADD COLUMN `reencryption` tinyint(1) DEFAULT NULL;
 ALTER TABLE lb_ssl ADD COLUMN `certificate_authority` mediumtext;
 
-update meta set `meta_value` = '64' where `meta_key`='version';
+update meta set `meta_value` = '??' where `meta_key`='version';
