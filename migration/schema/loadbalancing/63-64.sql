596e62fa54f1aecb4d4a503cb698bcf42dd5f248
Brandon Logan <bran3993@mmf05nfd58.rackspace.corp>
2014-05-01 11:29:50 -0500

    Fixed migration scripts to handle if tables do not exist.

--- migration/schema/loadbalancing/63-64.sql
+++ migration/schema/loadbalancing/63-64.sql
@@ -1,10 +1,11 @@
 use `loadbalancing`;
 
 RENAME TABLE ip_version TO deprecated_ip_version;
+RENAME TABLE host_backup TO deprecated_host_backup;
 
 ALTER TABLE access_list DROP COLUMN ip_version;
 
-DROP TABLE test;
+DROP TABLE IF EXISTS `test`;
 
 UPDATE `meta` SET `meta_value` = '64' WHERE `meta_key`='version';
 
--- migration/schema/loadbalancing/64-63.sql
+++ migration/schema/loadbalancing/64-63.sql
@@ -1,10 +1,11 @@
 use `loadbalancing`;
 
-RENAME TABLE deprecated_host_backup TO host_backup,
-             deprecated_ip_version TO ip_version;
+RENAME TABLE deprecated_host_backup TO host_backup;
+RENAME TABLE deprecated_ip_version TO ip_version;
 
 ALTER TABLE access_list ADD ip_version varchar(32) DEFAULT NULL;
 
+DROP TABLE IF EXISTS `test`;
 CREATE TABLE `test` (
   `k` int(11) NOT NULL,
   `v` int(11) NOT NULL,
