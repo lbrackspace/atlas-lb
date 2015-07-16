b8146cacfbdc8b661c57a8408a06f9cb668c9cec
Adam Harwell <flux.adam@gmail.com>
2015-07-16 14:46:25 -0700

    Add options for REST timeout and UsagePoller ignoring clusters
    Change-Id: I041e253208210bd0f3121210e613e18ab30c7a8f

--- rest-clients/stingray-rest-client/src/main/java/org/rackspace/stingray/client/config/ClientConfigKeys.java
+++ rest-clients/stingray-rest-client/src/main/java/org/rackspace/stingray/client/config/ClientConfigKeys.java
@@ -6,6 +6,8 @@ public enum ClientConfigKeys implements ConfigurationKey {
     stingray_base_uri,
     stingray_stats_base_uri,
     stingray_admin_user,
-    stingray_admin_key
+    stingray_admin_key,
+    stingray_read_timeout,
+    stingray_connect_timeout
 
 }
