7410ceb5da678393067097be9f8f08f8501e7fd1
Adam Harwell <flux.adam@gmail.com>
2015-09-22 16:48:21 -0500

    Workaround missing HM in sync call by also creating it before pool
    Change-Id: I565acd6ed42374383796af95e7e55a3a9256348a

--- adapter/zxtm-service/src/main/java/org/openstack/atlas/adapter/zxtm/ZxtmAdapterImpl.java
+++ adapter/zxtm-service/src/main/java/org/openstack/atlas/adapter/zxtm/ZxtmAdapterImpl.java
@@ -145,6 +145,11 @@ public class ZxtmAdapterImpl implements ReverseProxyLoadBalancerAdapter {
         final String name = ZxtmNameBuilder.genVSName(loadBalancer);
         final String rollBackMessage = "Sync load balancer request canceled.";
 
+        // Create a HM if it exists first, because the pool needs it in the next step
+        if (loadBalancer.getHealthMonitor() != null) {
+            updateHealthMonitor(config, loadBalancer);
+        }
+
         if (!Arrays.asList(serviceStubs.getPoolBinding().getPoolNames()).contains(name)) {
             LOG.debug(String.format("Adding node pool '%s'...", name));
             createNodePool(config, loadBalancer.getId(), loadBalancer.getAccountId(), loadBalancer.getNodes(), loadBalancer.getAlgorithm());
