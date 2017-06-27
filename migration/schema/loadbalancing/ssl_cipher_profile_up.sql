a6ce523de6e795e9044e146cd73757bdcee73282
Namrata Anand <nanand.2023@gmail.com>
2017-06-27 21:41:45 +0530

    CLB-295 and CLB-301 -Modified ciphers column as text field in ssl_cipher_profile table -Modified cipher_list column as text field in lb_ssl table -SslCipherProfile.java is modified for having ciphers attribute mapped to text field -SslTermination.java is modified for having cipherList attribute mapped to text field
    Change-Id: I50841296965f731d0cbd19daf2b6c77d57ead07d

--- migration/schema/loadbalancing/ssl_cipher_profile_up.sql
+++ migration/schema/loadbalancing/ssl_cipher_profile_up.sql
@@ -4,14 +4,14 @@ DROP TABLE IF EXISTS `ssl_cipher_profile`;
 
 CREATE TABLE `ssl_cipher_profile` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) NOT NULL,
-  `ciphers` varchar(1024),
+  `name` varchar(256) NOT NULL,
+  `ciphers` TEXT,
   `comments` varchar(256),
    PRIMARY KEY (`id`),
    UNIQUE KEY `name` (`name`)
    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `lb_ssl` ADD COLUMN `cipher_list` VARCHAR(1024);
-ALTER TABLE `lb_ssl` ADD COLUMN `cipher_profile` VARCHAR (128), ADD CONSTRAINT `ssl_cipher_profile_ibfk_1` FOREIGN KEY (`cipher_profile`) REFERENCES `ssl_cipher_profile` (`name`);
+ALTER TABLE `lb_ssl` ADD COLUMN `cipher_list` TEXT;
+ALTER TABLE `lb_ssl` ADD COLUMN `cipher_profile` VARCHAR (256), ADD CONSTRAINT `ssl_cipher_profile_ibfk_1` FOREIGN KEY (`cipher_profile`) REFERENCES `ssl_cipher_profile` (`name`);
 
 update `meta` set `meta_value` = '?' where `meta_key`='version';
