ccb19ae7859bc84f156e40ffe49572dd48d2010a
Namrata Anand <nanand.2023@gmail.com>
2017-06-27 21:41:45 +0530

    CLB-295 and CLB-301 -Modified ciphers column as text field in ssl_cipher_profile table -Modified cipher_list column as text field in lb_ssl table -SslCipherProfile.java is modified for having ciphers attribute mapped to text field -SslTermination.java is modified for having cipherList attribute mapped to text field
    Change-Id: I50841296965f731d0cbd19daf2b6c77d57ead07d

--- persistence/src/main/java/org/openstack/atlas/service/domain/entities/SslCipherProfile.java
+++ persistence/src/main/java/org/openstack/atlas/service/domain/entities/SslCipherProfile.java
@@ -19,7 +19,7 @@ public class SslCipherProfile extends Entity implements Serializable {
     @OneToMany(mappedBy = "cipherProfile" , cascade = CascadeType.ALL)
     List<SslTermination> sslTerminations = new ArrayList<SslTermination>();
 
-    @Column(name = "name", nullable = false,unique = true,length = 128)
+    @Column(name = "name", nullable = false,unique = true,length = 256)
     private String name;
 
     @Column(name = "ciphers", nullable = true, length = 65535)
