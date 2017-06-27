136bbb01eb3eb5a24d30959e7968bfd052c527a1
Namrata Anand <nanand.2023@gmail.com>
2017-06-27 21:41:45 +0530

    CLB-295 and CLB-301 code check in
    Change-Id: I5262a536ba245cd5143578bf43f78925f4d3dba1

---
+++ migration/schema/loadbalancing/ssl_cipher_profile_down.sql
@@ -0,0 +1,9 @@
+use `loadbalancing`;
+
+ALTER TABLE lb_ssl DROP foreign key `ssl_cipher_profile_ibfk_1`;
+
+ALTER TABLE lb_ssl DROP COLUMN cipher_profile;
+ALTER TABLE lb_ssl DROP COLUMN cipher_list;
+DROP TABLE IF EXISTS `ssl_cipher_profile`;
+
+update `meta` set `meta_value` = '?' where `meta_key`='version';
