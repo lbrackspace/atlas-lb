81e96ba91dbba9fc53e7c449f9b340e272d634a9
MadhaviLReddy <madhaviravi2002@gmail.com>
2017-06-29 15:16:49 +0530

    Initial draft for CLB-297 Create ssltermination/ciphers resource and endpoint.
    * Created /<tenant_id>/loadbalancers/<loadbalancer_id>/ssltermination/ end point in SslTerminationResource.java
    * Created the skeleton structure for Resource class and Repository classes: SslCipherProfileResource.java, SslCipherProfileRepository.java,
    * Created the skeleton structure for the service classes: SslCipherProfileService.java, SslCipherProfileServiceImpl.java
    
    Change-Id: I172e47636b5108f41204796698d331950598d3e0

---
+++ persistence/src/main/java/org/openstack/atlas/service/domain/repository/SslCipherProfileRepository.java
@@ -0,0 +1,82 @@
+package org.openstack.atlas.service.domain.repository;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openstack.atlas.service.domain.entities.*;
+import org.openstack.atlas.service.domain.exceptions.BadRequestException;
+import org.openstack.atlas.service.domain.exceptions.EntityNotFoundException;
+import org.openstack.atlas.service.domain.exceptions.ImmutableEntityException;
+import org.openstack.atlas.service.domain.exceptions.UnprocessableEntityException;
+import org.openstack.atlas.service.domain.util.Constants;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@org.springframework.stereotype.Repository
+@Transactional
+public class SslCipherProfileRepository {
+    final Log LOG = LogFactory.getLog(SslCipherProfileRepository.class);
+
+    @PersistenceContext(unitName = "loadbalancing")
+    private EntityManager entityManager;
+
+    public SslCipherProfile getById(Integer id) throws EntityNotFoundException {
+        SslCipherProfile profile = entityManager.find(SslCipherProfile.class, id);
+        if (profile == null) {
+            String errMsg = String.format("Cannot find the profile with id=%d", id);
+            LOG.warn(errMsg);
+            throw new EntityNotFoundException(errMsg);
+        }
+        return profile;
+    }
+
+    public SslCipherProfile getByName(String name) throws EntityNotFoundException {
+        List<SslCipherProfile> sslCipherProfiles = entityManager.createQuery("SELECT s FROM SslCipherProfile s where s.name = :name").setParameter("name", name).getResultList();
+        if (sslCipherProfiles != null && !sslCipherProfiles.isEmpty()) {
+            return sslCipherProfiles.get(0);
+        }
+        return null;
+    }
+
+    public List<SslCipherProfile> getMatchingByName(String name) throws EntityNotFoundException {
+        List<SslCipherProfile> sslCipherProfiles = entityManager.createQuery("SELECT s FROM SslCipherProfile s where s.name like :name").setParameter("name", '%'+name+'%').getResultList();
+        return sslCipherProfiles;
+    }
+
+    public List<SslCipherProfile> fetchAllProfiles() throws EntityNotFoundException {
+        List<SslCipherProfile> sslCipherProfiles = entityManager.createQuery("SELECT s FROM SslCipherProfile").getResultList();
+        return sslCipherProfiles;
+    }
+
+    public SslCipherProfile getCipherProfileForLoadBalancerId(Integer lbId) throws EntityNotFoundException {
+        return null;
+    }
+
+    public SslCipherProfile getCipherProfileForSslTerminationId(Integer sslTermId) throws EntityNotFoundException {
+        return null;
+    }
+
+    public void removeCipherProfileFromSslTermination(SslTermination sslTermination, Integer profileId) throws EntityNotFoundException {
+
+    }
+
+    public void deleteCipherProfile(SslCipherProfile sslCipherProfile) throws EntityNotFoundException {
+        /* Added the skeleton for future use for management api to delete a cipher profile. */
+    }
+
+    public void updateCipherProfile(SslCipherProfile sslCipherProfile) throws EntityNotFoundException, UnprocessableEntityException, BadRequestException, ImmutableEntityException {
+        /* Added the skeleton for future use for management api to update a cipher profile. */
+    }
+    public void saveCipherProfile(SslCipherProfile sslCipherProfile) {
+        /* Added the skeleton for future use for management api to create a cipher profile. */
+    }
+}
---
+++ persistence/src/main/java/org/openstack/atlas/service/domain/services/SslCipherProfileService.java
@@ -0,0 +1,20 @@
+package org.openstack.atlas.service.domain.services;
+
+import org.openstack.atlas.service.domain.entities.SslCipherProfile;
+import org.openstack.atlas.service.domain.entities.SslTermination;
+import org.openstack.atlas.service.domain.exceptions.*;
+
+public interface SslCipherProfileService {
+
+    SslCipherProfile getById(Integer id) throws EntityNotFoundException;
+
+    SslCipherProfile getCipherProfileByLoadBalancerId(Integer lbId) throws EntityNotFoundException;
+
+    SslCipherProfile getCipherProfileForSSLTerminationId(Integer SSLTermId) throws EntityNotFoundException;
+
+    void createCipherProfile(SslCipherProfile profile);
+
+    void updateCipherProfile(SslTermination sslTermination) throws EntityNotFoundException, UnprocessableEntityException, BadRequestException, ImmutableEntityException;
+
+    void removeCipherProfile(SslTermination sslTermination) throws EntityNotFoundException;
+}
