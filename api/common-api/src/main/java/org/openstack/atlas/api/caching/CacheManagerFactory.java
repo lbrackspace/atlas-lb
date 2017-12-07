bc633b0e730c1b1d2eb08d8b03493fac3fc76fdd
MadhaviLReddy <madhaviravi2002@gmail.com>
2017-12-07 19:13:57 +0530

    CLB-382 Upgrade ehcache dependency to latest or best compatible version
    Dependency is removed as it is not being used for Hibernate second level caching.
    Removed the classes: CacheRepository, CacheRepositoryImpl, CacheManagerFactory
    and AuthCacheTest as these are not used any where.
    
    Change-Id: I3e5c2c7d33555a3b15734e4d0e30e5d02e6a15b0

--- api/common-api/src/main/java/org/openstack/atlas/api/caching/CacheManagerFactory.java
+++
@@ -1,9 +0,0 @@
-package org.openstack.atlas.api.caching;
-
-import net.sf.ehcache.CacheManager;
-
-public class CacheManagerFactory {
-    public static CacheManager get() {
-        return CacheManager.create();
-    }
-}
