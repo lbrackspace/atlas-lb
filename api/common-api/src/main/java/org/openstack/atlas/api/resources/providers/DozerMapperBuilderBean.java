9ba015c8489b1a6a8156682097671e960fed56a6
MadhaviLReddy <madhaviravi2002@gmail.com>
2018-01-30 22:54:34 +0530

    CLB-467 Remove DozerBeanMapper usage in spring configuration
    Added new beans DozerMapperBuilderBean, MgmtDozerMapperBuilderBean.
    Added reference of the new beans in CommonDependencyProvider or ManagementDependencyProvider.
    Added init-method in CommonDependencyProvider or ManagementDependencyProvider to
    initialize the dozerMapper.
    
    Change-Id: I8df9a489e6b28e4cbd96ee463a08652c3dde7dba

---
+++ api/common-api/src/main/java/org/openstack/atlas/api/resources/providers/DozerMapperBuilderBean.java
@@ -0,0 +1,50 @@
+package org.openstack.atlas.api.resources.providers;
+
+import org.dozer.DozerBeanMapperBuilder;
+import org.dozer.DozerEventListener;
+import org.dozer.Mapper;
+
+import java.util.List;
+
+public class DozerMapperBuilderBean {
+
+    protected Mapper dozerMapperObject;
+
+    private List<String> mappingFiles;
+
+    private List<DozerEventListener> eventListeners;
+
+    public List<String> getMappingFiles() {
+        return mappingFiles;
+    }
+
+    public void setMappingFiles(List<String> mappingFiles) {
+        this.mappingFiles = mappingFiles;
+    }
+
+    public List<DozerEventListener> getEventListeners() {
+        return eventListeners;
+    }
+
+    public void setEventListeners(List<DozerEventListener> eventListeners) {
+        this.eventListeners = eventListeners;
+    }
+
+    public Mapper getDozerMapperObject() {
+        return dozerMapperObject;
+    }
+
+    public void setDozerMapperObject(Mapper dozerMapperObject) {
+        this.dozerMapperObject = dozerMapperObject;
+    }
+
+    public void init() throws Exception{
+        DozerBeanMapperBuilder dozerBeanMapperBuilder = DozerBeanMapperBuilder.create()
+                .withMappingFiles(mappingFiles.toArray(new String[mappingFiles.size()]));
+        for(DozerEventListener listener : eventListeners) {
+            //Multiple calls of this method will register multiple listeners in the order of calling
+            dozerBeanMapperBuilder = dozerBeanMapperBuilder.withEventListener(listener);
+        }
+        dozerMapperObject = dozerBeanMapperBuilder.build();
+    }
+}
