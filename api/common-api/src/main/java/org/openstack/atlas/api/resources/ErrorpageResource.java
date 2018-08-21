61a4054fcb8830e2dd1097867637f7fdc00a13a5
Namrata Anand <nanand.2023@gmail.com>
2018-08-21 14:31:31 +0530

    CLB-506 CLB custom error page doesn't work when first line contains a ":" -Adding "\n" to context to fix the issue of error page
    Change-Id: I0fafa55dc5685afd8e27a893f8560379c314a912

--- api/common-api/src/main/java/org/openstack/atlas/api/resources/ErrorpageResource.java
+++ api/common-api/src/main/java/org/openstack/atlas/api/resources/ErrorpageResource.java
@@ -83,8 +83,7 @@ public class ErrorpageResource extends CommonDependencyProvider{
         dataContainer = new MessageDataContainer();
         dataContainer.setAccountId(accountId);
         dataContainer.setLoadBalancerId(loadBalancerId);
-        //QuickFix for V1-D-12106
-//        content = "\n" + content;
+        content = "\n" + content;
         dataContainer.setErrorFileContents(content);
         try {
             asyncService.callAsyncLoadBalancingOperation(Operation.UPDATE_ERRORFILE, dataContainer);
