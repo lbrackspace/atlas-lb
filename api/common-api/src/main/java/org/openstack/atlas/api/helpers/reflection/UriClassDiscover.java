5f267999aab2bd562ed9cb089cf7ca9417c520b0
Carlos D. Garza <carlos.garza@rackspace.com>
2019-07-01 10:23:48 -0500

    Adding certmapping to bounce resource for validation(recommit)
    Change-Id: I67e18937b48a7ff68d956aa28fe282e72e72f554

--- api/common-api/src/main/java/org/openstack/atlas/api/helpers/reflection/UriClassDiscover.java
+++ api/common-api/src/main/java/org/openstack/atlas/api/helpers/reflection/UriClassDiscover.java
@@ -46,6 +46,7 @@ public class UriClassDiscover {
         getrList().add(new REFCN(".*\\d+/loadbalancers/bounce/updated(\\.json|\\.xml)?/?$", Updated.class));
         getrList().add(new REFCN(".*\\d+/loadbalancers/bounce/sslterminationvalidation(\\.json|\\.xml)?/?$", SslTermination.class));
         getrList().add(new REFCN(".*\\d+/loadbalancers/bounce/ssltermination(\\.json|\\.xml)?/?$", SslTermination.class));
+        getrList().add(new REFCN(".*\\d+/loadbalancers/bounce/certificatemapping(\\.json|\\.xml)?/?$", SslTermination.class));
         getrList().add(new REFCN(".*/management/hosts(\\.json|\\.xml)?/?$", Host.class));
     }
 
