0dec391cbc25a555cd5a26ff20953f856884aa42
dereknoblej <derek.noble@rackspace.com>
2020-02-07 10:13:27 -0600

    created versioned directories for testing
    Change-Id: Id8ead426a2577ed1bb5faa3f3171bf3f2f7039ed

---
+++ rest-clients/stingray-rest-client/src/test/java/org/rackspace/stingray/client/_3_4/mock/Path.java
@@ -0,0 +1,5 @@
+package org.rackspace.stingray.client._3_4.mock;
+
+public class Path {
+    private static final String ROOT_API_PATH = "https://localhost:9070/api/tm/3.4/config/active/";
+}
--- rest-clients/stingray-rest-client/src/test/java/org/rackspace/stingray/client/mock/Path.java
+++
@@ -1,5 +0,0 @@
-package org.rackspace.stingray.client.mock;
-
-public class Path {
-    private static final String ROOT_API_PATH = "https://localhost:9070/api/tm/3.4/config/active/";
-}
