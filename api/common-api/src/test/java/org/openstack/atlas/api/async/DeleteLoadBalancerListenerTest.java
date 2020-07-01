087853c3cd1b516e251c7a1b871af1f9aca2ca10
phillip-toohill <phillip.toohill@rackspace.com>
2020-07-01 15:48:02 -0500

    Handle shreadVip vTM traffic ip group references
    Change-Id: I249523ed89b2ebe06e6c8ebee10d9dbf8eb41e50

--- api/common-api/src/test/java/org/openstack/atlas/api/async/DeleteLoadBalancerListenerTest.java
+++ api/common-api/src/test/java/org/openstack/atlas/api/async/DeleteLoadBalancerListenerTest.java
@@ -1,6 +1,7 @@
 package org.openstack.atlas.api.async;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Matchers;
@@ -10,8 +11,7 @@ import org.openstack.atlas.api.async.util.STMTestBase;
 import org.openstack.atlas.api.integration.ReverseProxyLoadBalancerVTMService;
 import org.openstack.atlas.cfg.ConfigurationKey;
 import org.openstack.atlas.cfg.RestApiConfiguration;
-import org.openstack.atlas.service.domain.entities.LoadBalancerStatus;
-import org.openstack.atlas.service.domain.entities.SslTermination;
+import org.openstack.atlas.service.domain.entities.*;
 import org.openstack.atlas.service.domain.events.entities.CategoryType;
 import org.openstack.atlas.service.domain.events.entities.EventSeverity;
 import org.openstack.atlas.service.domain.events.entities.EventType;
@@ -26,7 +26,9 @@ import org.openstack.atlas.usagerefactor.collection.UsageEventCollection;
 
 import javax.jms.ObjectMessage;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
@@ -92,6 +94,67 @@ public class DeleteLoadBalancerListenerTest extends STMTestBase {
 
         deleteLoadBalancerListener.doOnMessage(objectMessage);
 
+        verify(loadBalancerService, times(1)).isSharedVip4(lb, vip1);
+        verify(loadBalancerService, times(0)).isSharedVip6(any(), any());
+        Assert.assertEquals(1, lb.getLoadBalancerJoinVipSet().size());
+        verify(usageEventCollection).getUsage(lb);
+        verify(reverseProxyLoadBalancerVTMService).deleteLoadBalancer(lb);
+        verify(loadBalancerService).pseudoDelete(lb);
+        verify(loadBalancerStatusHistoryService).save(ACCOUNT_ID, LOAD_BALANCER_ID, LoadBalancerStatus.DELETED);
+        verify(notificationService).saveLoadBalancerEvent(eq(USERNAME), eq(ACCOUNT_ID), eq(LOAD_BALANCER_ID), anyString(), anyString(), eq(EventType.DELETE_LOADBALANCER), eq(CategoryType.DELETE), eq(EventSeverity.INFO));
+    }
+
+    @Test
+    public void testDeleteValidLoadBalancerWithSharedVip4() throws Exception {
+        lb.setSslTermination(null);
+        when(objectMessage.getObject()).thenReturn(lb);
+        when(loadBalancerService.getWithUserPages(LOAD_BALANCER_ID, ACCOUNT_ID)).thenReturn(lb);
+        when(usageEventCollection.getUsage(lb)).thenReturn(usages);
+        when(loadBalancerService.pseudoDelete(lb)).thenReturn(lb);
+        when(config.getString(Matchers.<ConfigurationKey>any())).thenReturn("REST");
+        when(loadBalancerService.isSharedVip4(any(), any())).thenReturn(true);
+
+        deleteLoadBalancerListener.doOnMessage(objectMessage);
+
+        verify(loadBalancerService, times(1)).isSharedVip4(lb, vip1);
+        verify(loadBalancerService, times(0)).isSharedVip6(any(), any());
+        // vips on dbLoadbalancer will remain set so db is purged properly
+        Assert.assertEquals(1, lb.getLoadBalancerJoinVipSet().size());
+        verify(usageEventCollection).getUsage(lb);
+        verify(reverseProxyLoadBalancerVTMService).deleteLoadBalancer(lb);
+        verify(loadBalancerService).pseudoDelete(lb);
+        verify(loadBalancerStatusHistoryService).save(ACCOUNT_ID, LOAD_BALANCER_ID, LoadBalancerStatus.DELETED);
+        verify(notificationService).saveLoadBalancerEvent(eq(USERNAME), eq(ACCOUNT_ID), eq(LOAD_BALANCER_ID), anyString(), anyString(), eq(EventType.DELETE_LOADBALANCER), eq(CategoryType.DELETE), eq(EventSeverity.INFO));
+    }
+
+    @Test
+    public void testDeleteValidLoadBalancerWithSharedVip6() throws Exception {
+        Set<LoadBalancerJoinVip6> vipList = new HashSet<>();
+        VirtualIpv6 vip61 = new VirtualIpv6();
+        vip61.setId(13);
+        vip61.setVipOctets(16);
+        LoadBalancerJoinVip6 loadBalancerJoinVip = new LoadBalancerJoinVip6();
+        loadBalancerJoinVip.setVirtualIp(vip61);
+        loadBalancerJoinVip.setId(new LoadBalancerJoinVip6.Id(TEST_LOADBALANCER_ID, 13));
+        vipList.add(loadBalancerJoinVip);
+        lb.setLoadBalancerJoinVip6Set(vipList);
+
+        lb.setSslTermination(null);
+        when(objectMessage.getObject()).thenReturn(lb);
+        when(loadBalancerService.getWithUserPages(LOAD_BALANCER_ID, ACCOUNT_ID)).thenReturn(lb);
+        when(usageEventCollection.getUsage(lb)).thenReturn(usages);
+        when(loadBalancerService.pseudoDelete(lb)).thenReturn(lb);
+        when(config.getString(Matchers.<ConfigurationKey>any())).thenReturn("REST");
+        when(loadBalancerService.isSharedVip4(any(), any())).thenReturn(false);
+        when(loadBalancerService.isSharedVip6(any(), any())).thenReturn(true);
+
+        deleteLoadBalancerListener.doOnMessage(objectMessage);
+
+        verify(loadBalancerService, times(1)).isSharedVip4(lb, vip1);
+        verify(loadBalancerService, times(1)).isSharedVip6(lb, vip61);
+        // vips on dbLoadbalancer will remain set so db is purged properly
+        Assert.assertEquals(1, lb.getLoadBalancerJoinVipSet().size());
+        Assert.assertEquals(1, lb.getLoadBalancerJoinVip6Set().size());
         verify(usageEventCollection).getUsage(lb);
         verify(reverseProxyLoadBalancerVTMService).deleteLoadBalancer(lb);
         verify(loadBalancerService).pseudoDelete(lb);
