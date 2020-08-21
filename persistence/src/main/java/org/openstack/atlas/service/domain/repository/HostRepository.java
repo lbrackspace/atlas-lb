9aaaebf75188848f3a99f1c24bacc9e3133cf667
dereknoblej <derek.noble@rackspace.com>
2020-08-21 11:58:05 -0600

    update to make lb not get reassigned to same host
    Change-Id: Ie288d92fc921e36f6831c37c40f90eb80203b2ec

--- persistence/src/main/java/org/openstack/atlas/service/domain/repository/HostRepository.java
+++ persistence/src/main/java/org/openstack/atlas/service/domain/repository/HostRepository.java
@@ -420,12 +420,12 @@ public class HostRepository {
         return lbs;
     }
 
-    public Host getDefaultActiveHost(Integer clusterId) throws EntityNotFoundException {
+    public Host getDefaultActiveHost(Integer clusterId, Integer hostId) throws EntityNotFoundException {
         //get a host based on the following algorithm
         //status = ACTIVE_TARGET, fewest concurrent connections and fewest number of assigned loadbalanders.
-        String sql = "SELECT h from Host h where h.cluster.id = :clusterId AND h.hostStatus= :hostStatus "
+        String sql = "SELECT h from Host h where h.cluster.id = :clusterId AND h.hostStatus= :hostStatus AND h.id <> :hostId "
                 + "AND h.maxConcurrentConnections = (select min(i.maxConcurrentConnections) "
-                + "from Host i where i.cluster.id = :clusterId AND i.hostStatus = :hostStatus)";
+                + "from Host i where i.cluster.id = :clusterId AND i.hostStatus = :hostStatus AND i.id <> :hostId)";
 
 //        String sql = "SELECT h from Host h where h.cluster.id = :clusterId"
 //                + " AND h.hostStatus = '"
@@ -433,7 +433,7 @@ public class HostRepository {
 //                + " AND h.maxConcurrentConnections =  ( select min(i.maxConcurrentConnections) from Host i where i.hostStatus = '"
 //                + HostStatus.ACTIVE_TARGET + "')";
 
-        Query qry = entityManager.createQuery(sql).setParameter("hostStatus", HostStatus.ACTIVE_TARGET).setParameter("clusterId", clusterId);
+        Query qry = entityManager.createQuery(sql).setParameter("hostStatus", HostStatus.ACTIVE_TARGET).setParameter("clusterId", clusterId).setParameter("hostId", hostId);
         List<Host> hosts = qry.getResultList();
 
         if (hosts != null && hosts.size() > 0) {
@@ -467,4 +467,46 @@ public class HostRepository {
         }
         throw new EntityNotFoundException("ACTIVE_TARGET host not found");
     }
+
+//    public Host getNotCurrentActiveHost(Integer clusterId, Integer hostId) throws EntityNotFoundException {
+//        //get a host based on the following algorithm
+//        //status = ACTIVE_TARGET, fewest concurrent connections and fewest number of assigned loadbalanders.
+//        String sql = "SELECT h from Host h where h.cluster.id = :clusterId AND h.hostStatus= :hostStatus AND h.id <> :hostId "
+//                + "AND h.maxConcurrentConnections = (select min(i.maxConcurrentConnections) "
+//                + "from Host i where i.cluster.id = :clusterId AND i.hostStatus = :hostStatus AND i.id <> :hostId)";
+//
+//        Query qry = entityManager.createQuery(sql).setParameter("hostStatus", HostStatus.ACTIVE_TARGET).setParameter("clusterId", clusterId).setParameter("hostId", hostId);
+//        List<Host> hosts = qry.getResultList();
+//
+//        if (hosts != null && hosts.size() > 0) {
+//            if (hosts.size() == 1) {
+//                return (hosts.get(0));
+//            } else {
+//                Host minhost = null;
+//                long mincount = 0;
+//                long count = 0;
+//                //fewest number
+//                for (Host h : hosts) {
+//                    String query = "select count(*) from LoadBalancer lb where lb.host.id = :id and lb.status != 'DELETED'";
+//                    List<Long> lst = entityManager.createQuery(query).setParameter("id", h.getId()).getResultList();
+//                    count = lst.get(0).longValue();
+//                    if (count == 0) {
+//                        return h;
+//                    } else {
+//                        if (mincount == 0) {
+//                            mincount = count;
+//                            minhost = h;
+//                        } else if (mincount <= count) {
+//                            //do nothing
+//                        } else {
+//                            mincount = count;
+//                            minhost = h;
+//                        }
+//                    }
+//                }
+//                return minhost;
+//            }
+//        }
+//        throw new EntityNotFoundException("ACTIVE_TARGET host not found");
+//    }
 }
