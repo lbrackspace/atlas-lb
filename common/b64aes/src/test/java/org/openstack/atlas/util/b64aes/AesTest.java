f2e44968ea4d20cec64fb59d0caa472b55408e26
phillip-toohill <phillip.toohill@rackspace.com>
2020-09-09 09:42:59 -0500

    Updating Aes util with GCM based methods
    Change-Id: I1e364d420bc44b902376560cd864e9bd1764d3ee

---
+++ common/b64aes/src/test/java/org/openstack/atlas/util/b64aes/AesTest.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openstack.atlas.util.b64aes;
+
+import org.junit.*;
+import org.omg.CORBA.DynAnyPackage.Invalid;
+
+import javax.crypto.AEADBadTagException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import static org.junit.Assert.*;
+
+public class AesTest {
+
+    public AesTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws UnsupportedEncodingException {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGCMEncryptAndDecryptToChar() throws NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, IOException, InvalidKeySpecException {
+        String key = "nZr4u7x!A%D*F-Ja";
+        String test = "testt";
+        String ctest = "encryptMe";
+        String iv = "testiv";
+        String etest = Aes.b64encryptGCM(ctest.getBytes(), key, iv);
+        String dtest = Aes.b64decryptGCM_str(etest, key, iv);
+        assertEquals(ctest, dtest);
+    }
+
+    @Test
+    public void testGCMEncryptAndDecryptNotEquals() throws NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, IOException, InvalidKeySpecException {
+        String key = "nZr4u7x!A%D*F-Ja";
+        String test = "testt";
+        String ctest = "encryptMe";
+        String iv = "testiv";
+        String etest = Aes.b64encryptGCM(ctest.getBytes(), key, iv);
+        String dtest = Aes.b64decryptGCM_str(etest, key, iv);
+        assertNotEquals(test, dtest);
+    }
+
+    @Test(expected = AEADBadTagException.class)
+    public void testGCMDecryptWithInvalidKey() throws NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, IOException, InvalidKeySpecException {
+        String key = "nZr4u7x!A%D*F-Ja";
+        String invalidKey = "nZr4u7x!A%D*F-Jb";
+        String ctest = "encryptMe";
+        String iv = "testiv";
+        String etest = Aes.b64encryptGCM(ctest.getBytes(), key, iv);
+        String dtest = Aes.b64decryptGCM_str(etest, invalidKey, iv);
+        assertEquals(ctest, dtest);
+    }
+
+    @Test(expected = AEADBadTagException.class)
+    public void testGCMDecryptIncorrectIV() throws NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, IOException, InvalidKeySpecException {
+        String key = "nZr4u7x!A%D*F-Ja";
+        String incorrectIV = "testt";
+        String ctest = "encryptMe";
+        String iv = "testiv";
+        String etest = Aes.b64encryptGCM(ctest.getBytes(), key, iv);
+        String dtest = Aes.b64decryptGCM_str(etest, key, incorrectIV);
+        assertEquals(ctest, dtest);
+    }
+
+    @Test
+    public void testEncryptOutputDoesNotEqualInput() throws NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, IOException, InvalidKeySpecException {
+        String key = "nZr4u7x!A%D*F-Ja";
+        String ctest = "encryptMe";
+        String iv = "testiv";
+        String etest = Aes.b64encryptGCM(ctest.getBytes(), key, iv);
+        Assert.assertNotEquals(ctest, etest);
+    }
+
+}
