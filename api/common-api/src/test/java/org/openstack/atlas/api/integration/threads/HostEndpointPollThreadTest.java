2d13a7473abb6aa52058587d2e592a1cfe6a8c8e
phillip-toohill <phillip.toohill@rackspace.com>
2020-12-16 13:08:27 -0600

    Updating hostEndpointPollerThread for restEndpoint toString
    Change-Id: I8eeb16f406115f88b112bde815c67f455838b7f8

--- api/common-api/src/test/java/org/openstack/atlas/api/integration/threads/HostEndpointPollThreadTest.java
+++ api/common-api/src/test/java/org/openstack/atlas/api/integration/threads/HostEndpointPollThreadTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.openstack.atlas.api.integration.ReverseProxyLoadBalancerVTMService;
+import org.openstack.atlas.service.domain.entities.Host;
 
 import static org.mockito.Mockito.*;
 
@@ -37,22 +38,36 @@ public class HostEndpointPollThreadTest {
     public void testPollEndpointsActive() throws Exception {
         when(reverseProxyLoadBalancerVTMService.isEndPointWorking(any())).thenReturn(Boolean.TRUE);
 
-        hostEndpointPollThread.setEndPointWorking(false);
-        hostEndpointPollThread.setRestEndPointWorking(false);
         hostEndpointPollThread.run();
         verify(reverseProxyLoadBalancerVTMService, times(1)).isEndPointWorking(any());
         Assert.assertTrue(hostEndpointPollThread.isRestEndPointWorking());
+        Assert.assertFalse(hostEndpointPollThread.isEndPointWorking());
+    }
+
+    @Test
+    public void testPollEndpointThreadToString() throws Exception {
+        Host h = new Host();
+        h.setId(1);
+        h.setRestEndpoint("http://restendpoint.co/config/active");
+
+        when(reverseProxyLoadBalancerVTMService.isEndPointWorking(any())).thenReturn(Boolean.TRUE);
+        hostEndpointPollThread.setHost(h);
+        hostEndpointPollThread.run();
+        String expectedString = "HostEndpointPollThread{ host_id=1, url=http://restendpoint.co/config/active}";
+        Assert.assertEquals(expectedString, hostEndpointPollThread.toString());
+        verify(reverseProxyLoadBalancerVTMService, times(1)).isEndPointWorking(any());
+        Assert.assertTrue(hostEndpointPollThread.isRestEndPointWorking());
+        Assert.assertFalse(hostEndpointPollThread.isEndPointWorking());
     }
 
     @Test
     public void testPollEndpointsInActive() throws Exception {
         when(reverseProxyLoadBalancerVTMService.isEndPointWorking(any())).thenReturn(Boolean.FALSE);
 
-        hostEndpointPollThread.setEndPointWorking(true);
-        hostEndpointPollThread.setRestEndPointWorking(true);
         hostEndpointPollThread.run();
         verify(reverseProxyLoadBalancerVTMService, times(1)).isEndPointWorking(any());
         Assert.assertFalse(hostEndpointPollThread.isRestEndPointWorking());
+        Assert.assertFalse(hostEndpointPollThread.isEndPointWorking());
     }
 
     @Test
