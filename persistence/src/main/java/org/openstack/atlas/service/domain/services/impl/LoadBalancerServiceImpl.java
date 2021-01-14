256ea84f7b6563318d550f1e423243175ac712c6
phillip-toohill <phillip.toohill@rackspace.com>
2021-01-14 10:25:11 -0600

    Adding service layer method for retrieving cluster source addy info
    Change-Id: I0b8d82731c21280421836b26c10137ae0c24f4df

--- persistence/src/main/java/org/openstack/atlas/service/domain/services/impl/LoadBalancerServiceImpl.java
+++ persistence/src/main/java/org/openstack/atlas/service/domain/services/impl/LoadBalancerServiceImpl.java
@@ -2,6 +2,7 @@ package org.openstack.atlas.service.domain.services.impl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openstack.atlas.docs.loadbalancers.api.v1.ClusterSourceAddresses;
 import org.openstack.atlas.docs.loadbalancers.api.v1.ProtocolPortBindings;
 import org.openstack.atlas.service.domain.cache.AtlasCache;
 import org.openstack.atlas.service.domain.deadlock.DeadLockRetry;
@@ -470,6 +471,19 @@ public class LoadBalancerServiceImpl extends BaseService implements LoadBalancer
                 throw new BadRequestException(ccErr);
             }
         }
+    }
+
+    @Transactional
+    public ClusterSourceAddresses getClusterSourceAddresses(Integer clusterId) {
+        List<Host> hosts = hostRepository.getAllHostsByClusterId(clusterId);
+        ClusterSourceAddresses csa = new ClusterSourceAddresses();
+        for (Host h : hosts) {
+            csa.getIpv4Publicnets().add(h.getIpv4Public());
+            csa.getIpv4Servicenets().add(h.getIpv4Servicenet());
+            csa.getIpv6Publicnets().add(h.getIpv6Public());
+            csa.getIpv6Servicenets().add(h.getIpv6Servicenet());
+        }
+        return csa;
     }
 
     @Transactional
