c15f7d2c5f1b71c9d2dd9961e0f6385bc6c32f8a
akas8959 <akash.bhaskar@rackspace.com>
2021-02-10 17:59:43 +0530

    Added delete functionality for clusters
    Change-Id: Ia38e78bdea6d641670588062e130ffe13f55113f

--- persistence/src/test/java/org/openstack/atlas/service/domain/services/ClusterServiceImplTest.java
+++ persistence/src/test/java/org/openstack/atlas/service/domain/services/ClusterServiceImplTest.java
@@ -1,19 +1,26 @@
 package org.openstack.atlas.service.domain.services;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.experimental.runners.Enclosed;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.openstack.atlas.service.domain.entities.Cluster;
 import org.openstack.atlas.service.domain.entities.ClusterType;
+import org.openstack.atlas.service.domain.entities.Host;
+import org.openstack.atlas.service.domain.exceptions.ClusterNotEmptyException;
 import org.openstack.atlas.service.domain.repository.ClusterRepository;
 import org.openstack.atlas.service.domain.services.impl.ClusterServiceImpl;
 
-import static org.mockito.Mockito.doReturn;
+import javax.xml.ws.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 @RunWith(Enclosed.class)
 public class ClusterServiceImplTest {
@@ -38,5 +45,50 @@ public class ClusterServiceImplTest {
            cType = clusterService.getClusterTypeByAccountId(1);
            Assert.assertEquals(cType, ClusterType.STANDARD);
        }
+   }
+
+   public static class whenDeletingCluster{
+
+       @Mock
+       private ClusterRepository clusterRepository;
+       @Mock
+       private ClusterService clusterServiceMock;
+       @InjectMocks
+       private ClusterServiceImpl clusterService;
+       private Cluster cluster;
+       private Response response;
+       private Host host;
+       private List<Host> hosts;
+
+       @Rule
+       public final ExpectedException expectedException = ExpectedException.none();
+
+       @Before
+       public void standUp(){
+           MockitoAnnotations.initMocks(this);
+           cluster = new Cluster();
+           cluster.setId(1);
+           host = new Host();
+           host.setCluster(cluster);
+           hosts = new ArrayList<>();
+           hosts.add(host);
+       }
+
+       @Test
+       public void shouldReturn200StatusCodeAfterDeletion() throws ClusterNotEmptyException {
+           hosts.clear();
+           doReturn(hosts).when(clusterRepository).getHosts(ArgumentMatchers.anyInt());
+           clusterService.deleteCluster(cluster);
+           verify(clusterRepository, times(1)).delete(cluster);
+       }
+
+       @Test
+       public void shouldThrowClusterNotEmptyExceptionWhenClusterHasHostAssociated() throws ClusterNotEmptyException {
+           doReturn(hosts).when(clusterRepository).getHosts(ArgumentMatchers.anyInt());
+           expectedException.expect(ClusterNotEmptyException.class);
+           clusterService.deleteCluster(cluster);
+       }
+
+
    }
 }
