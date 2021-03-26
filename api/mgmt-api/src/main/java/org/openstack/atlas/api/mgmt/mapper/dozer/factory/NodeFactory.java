dbb1efe01040a8c9830688f50675d7b04b9c653c
dereknoblej <derek.noble@rackspace.com>
2021-03-26 13:21:04 -0500

    update/delete node
    Change-Id: Ic4623187717d9fd43667d5dd71ad2736b8b0aa66

---
+++ api/mgmt-api/src/main/java/org/openstack/atlas/api/mgmt/mapper/dozer/factory/NodeFactory.java
@@ -0,0 +1,33 @@
+package org.openstack.atlas.api.mgmt.mapper.dozer.factory;
+
+import org.dozer.BeanFactory;
+import org.dozer.config.BeanContainer;
+import org.openstack.atlas.docs.loadbalancers.api.management.v1.Node;
+
+public class NodeFactory implements BeanFactory {
+
+    @Override
+    public Object createBean(Object source, Class<?> sourceClass, String targetBeanId, BeanContainer beanContainer) {
+
+        if (sourceClass.equals(org.openstack.atlas.service.domain.entities.Node.class)) {
+            Node node = new Node();
+            node.setId(null);
+            node.setAddress(null);
+            node.setCondition(null);
+            node.setPort(null);
+            node.setStatus(null);
+            node.setType(null);
+            node.setWeight(null);
+            return node;
+        }
+
+        if (sourceClass.equals(Node.class)) {
+            return new org.openstack.atlas.service.domain.entities.Node();
+        }
+
+        return null;
+    }
+
+
+
+}
