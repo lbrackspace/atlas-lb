4b7f0c062a501410b7c4849326acda2e821c1b1c
phillip-toohill <phillip.toohill@rackspace.com>
2021-05-04 15:33:55 -0500

    Updating atlasCache to better handle config updates
    Change-Id: Icf0c9aa5244177a8f885d0d3b756da1fbee62481

--- persistence/src/main/java/org/openstack/atlas/service/domain/cache/AtlasCache.java
+++ persistence/src/main/java/org/openstack/atlas/service/domain/cache/AtlasCache.java
@@ -16,22 +16,16 @@ import java.io.IOException;
 @Component
 public class AtlasCache {
     private final Log LOG = LogFactory.getLog(AtlasCache.class);
-    private static MemcachedClient cacheClient;
+
+    private RestApiConfiguration configuration;
+    private MemcachedClient cacheClient;
     private String ttl = "300";
+    private String cacheHosts;
 
     @Autowired
     public AtlasCache(RestApiConfiguration configuration) throws IOException {
-        String cacheHosts;
-        //need to get config from a common place..
-        if (configuration.hasKeys(ServicesConfigurationKeys.memcached_servers)) {
-            cacheHosts = configuration.getString(ServicesConfigurationKeys.memcached_servers);
-            buildCacheInstance(cacheHosts);
-        } else {
-            throw new MissingFieldException("The cache server host(s) information could not be found for the current configuration.");
-        }
-        if (configuration.hasKeys(ServicesConfigurationKeys.ttl)) {
-            this.ttl = configuration.getString(ServicesConfigurationKeys.ttl);
-        }
+        this.configuration = configuration;
+        loadClient();
     }
 
     /**
@@ -66,15 +60,18 @@ public class AtlasCache {
      * @return return the object
      */
     public Object get(String key) {
+
         Object o = null;
         try {
+            checkState();
+
             o = cacheClient.get(key);
             if (o == null) {
                 //Sometimes connection is stale
                 LOG.info("Cache miss Retrying...");
                 o = cacheClient.get(key);
             }
-        } catch (RuntimeException ex) {
+        } catch (Exception ex) {
             LOG.info("Server is down...");
         }
         return o;
@@ -117,8 +114,49 @@ public class AtlasCache {
      * @throws java.io.IOException
      */
     private void buildCacheInstance(String cacheHosts) throws IOException {
+        LOG.info("Building memcached client...");
         cacheClient = new MemcachedClient(
                 new BinaryConnectionFactory(),
                 AddrUtil.getAddresses(cacheHosts));
+    }
+
+    /**
+     * @throws IOException
+     */
+    private void loadClient() throws IOException {
+        LOG.info("Loading memcached client configurations");
+
+        //need to get config from a common place..
+        if (configuration.hasKeys(ServicesConfigurationKeys.memcached_servers)) {
+            this.cacheHosts = configuration.getString(ServicesConfigurationKeys.memcached_servers);
+            buildCacheInstance(cacheHosts);
+        } else {
+            throw new MissingFieldException("The cache server host(s) information " +
+                    "could not be found for the current configuration.");
+        }
+        if (configuration.hasKeys(ServicesConfigurationKeys.ttl)) {
+            this.ttl = configuration.getString(ServicesConfigurationKeys.ttl);
+        }
+    }
+
+    /**
+     * @throws IOException
+     */
+    private void rebuildClient() throws IOException {
+        LOG.info("Shutting down memcached client...");
+        cacheClient.shutdown();
+        loadClient();
+    }
+
+    /**
+     * @throws IOException
+     */
+    private void checkState() throws IOException {
+        LOG.info("Checking for modified configurations...");
+        if (!configuration.getString(ServicesConfigurationKeys.memcached_servers).equals(cacheHosts)
+                || !configuration.getString(ServicesConfigurationKeys.ttl).equals(this.ttl)) {
+            LOG.info("Detected modification, reload memcached client");
+            rebuildClient();
+        }
     }
 }
