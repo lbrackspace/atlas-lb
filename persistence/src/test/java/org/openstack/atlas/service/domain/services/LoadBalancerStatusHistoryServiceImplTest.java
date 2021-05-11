59929006e261ef3dd3e7b98b05469d2e93522584
dereknoblej <derek.noble@rackspace.com>
2021-05-11 14:03:35 -0500

    delete lb status history job
    Change-Id: I8601b787e4fc19f0147e0217663bfca90c45f547

---
+++ persistence/src/test/java/org/openstack/atlas/service/domain/services/LoadBalancerStatusHistoryServiceImplTest.java
@@ -0,0 +1,50 @@
+package org.openstack.atlas.service.domain.services;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openstack.atlas.service.domain.exceptions.EntityNotFoundException;
+import org.openstack.atlas.service.domain.repository.LoadBalancerStatusHistoryRepository;
+import org.openstack.atlas.service.domain.services.impl.LoadBalancerStatusHistoryServiceImpl;
+import java.util.Calendar;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.any;
+
+@RunWith(Enclosed.class)
+public class LoadBalancerStatusHistoryServiceImplTest {
+
+    public static class whenDeletingOldLBStatusHistory {
+
+        LoadBalancerStatusHistoryServiceImpl loadBalancerStatusHistoryService;
+        @Mock
+        LoadBalancerStatusHistoryRepository loadBalancerStatusHistoryRepository;
+        Calendar cal;
+
+        @Before
+        public void setUp() {
+            MockitoAnnotations.initMocks(this);
+            loadBalancerStatusHistoryService = new LoadBalancerStatusHistoryServiceImpl();
+            cal = Calendar.getInstance();
+            cal.add(Calendar.DATE, -180);
+            loadBalancerStatusHistoryService.setLoadBalancerStatusHistoryRepository(loadBalancerStatusHistoryRepository);
+        }
+
+        @Test
+        public void shouldSendCalendarOlderThan180DaysToLbStatusHistRepository() throws Exception {
+            loadBalancerStatusHistoryService.deleteLBStatusHistoryOlderThanSixMonths();
+            verify(loadBalancerStatusHistoryRepository, times(1)).batchDeleteStatusHistoryForLBOlderThanSixMonths(cal);
+        }
+
+        @Test(expected = EntityNotFoundException.class)
+        public void shouldThrowEntityNotFound() throws EntityNotFoundException {
+            doThrow(EntityNotFoundException.class).when(loadBalancerStatusHistoryRepository).batchDeleteStatusHistoryForLBOlderThanSixMonths(any());
+            loadBalancerStatusHistoryService.deleteLBStatusHistoryOlderThanSixMonths();
+        }
+    }
+}
