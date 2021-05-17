1ae61b59c3eaed0f8a21da66a8dce7a7f213395e
dereknoblej <derek.noble@rackspace.com>
2021-05-17 11:16:17 -0500

    updated blacklistResourceTest
    Change-Id: I8233a81101ee6f20b7324bf9c96661f897f2079b

--- api/mgmt-api/src/test/java/org/openstack/atlas/api/mgmt/resources/BlackListResourceTest.java
+++ api/mgmt-api/src/test/java/org/openstack/atlas/api/mgmt/resources/BlackListResourceTest.java
@@ -1,10 +1,13 @@
 package org.openstack.atlas.api.mgmt.resources;
 
 import org.dozer.DozerBeanMapperBuilder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.openstack.atlas.docs.loadbalancers.api.management.v1.Blacklist;
 import org.openstack.atlas.docs.loadbalancers.api.management.v1.BlacklistItem;
 import org.openstack.atlas.docs.loadbalancers.api.management.v1.BlacklistType;
 import org.openstack.atlas.docs.loadbalancers.api.management.v1.IpVersion;
+import org.openstack.atlas.service.domain.entities.Node;
 import org.openstack.atlas.service.domain.operations.OperationResponse;
 import org.openstack.atlas.api.mgmt.integration.ManagementAsyncService;
 import org.junit.Assert;
@@ -14,12 +17,16 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.openstack.atlas.service.domain.repository.BlacklistRepository;
 import org.openstack.atlas.service.domain.services.BlackListService;
+import org.openstack.atlas.util.ip.exception.IPStringConversionException;
+import org.openstack.atlas.util.ip.exception.IpTypeMissMatchException;
 
 import javax.ws.rs.core.Response;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 @RunWith(Enclosed.class)
 public class BlackListResourceTest {
@@ -63,6 +70,7 @@ public class BlackListResourceTest {
         private OperationResponse operationResponse;
         private Blacklist bl;
         private BlacklistItem bli;
+        private BlacklistItem bli2;
 
 
         @Before
@@ -85,9 +93,13 @@ public class BlackListResourceTest {
         public void standUpBlackList() {
             bl = new Blacklist();
             bli = new BlacklistItem();
+            bli2 = new BlacklistItem();
             bli.setCidrBlock("192.0.0.0/32");
+            bli2.setCidrBlock("192.5.0.0/32");
             bli.setIpVersion(IpVersion.IPV4);
+            bli2.setIpVersion(IpVersion.IPV4);
             bli.setType(BlacklistType.NODE);
+            bli2.setType(BlacklistType.NODE);
             bl.getBlacklistItems().add(bli);
         }
 
@@ -107,9 +119,151 @@ public class BlackListResourceTest {
         }
 
         @Test
+        public void shouldReturn400WhenCidrBlockIsInvalid() {
+            bli.setCidrBlock("0.0.0.0");
+            Response resp = blackListResource.addBlacklistItem(bl);
+            Assert.assertEquals(400, resp.getStatus());
+        }
+
+        @Test
+        public void shouldReturn400WhenBlacklistIsEmpty() {
+            bl.getBlacklistItems().clear();
+            Response resp = blackListResource.addBlacklistItem(bl);
+            Assert.assertEquals(400, resp.getStatus());
+        }
+
+        @Test
         public void shouldReturnOKWhenExecutedOkay() throws Exception {
             Response resp = blackListResource.addBlacklistItem(bl);
             Assert.assertEquals(202, resp.getStatus());
         }
+
+        @Test
+        public void shouldReturn400WhenDuplicatesAreInList() throws Exception {
+            bl.getBlacklistItems().add(bli);
+            Response response = blackListResource.addBlacklistItem(bl);
+            Assert.assertEquals(400, response.getStatus());
+        }
+
+        @Test
+        public void shouldReturn400WhenBlackListItemHasId() throws Exception {
+            bli.setId(1);
+            Response response = blackListResource.addBlacklistItem(bl);
+            Assert.assertEquals(400, response.getStatus());
+        }
+
+        @Test
+        public void shouldReturn202WhenListHasTwoNonDuplicates() {
+            bl.getBlacklistItems().add(bli2);
+            Response response = blackListResource.addBlacklistItem(bl);
+            Assert.assertEquals(202, response.getStatus());
+        }
+
+        @Test
+        public void shouldReturn202WhenListUsingIPV6HasTwoNonDuplicates() {
+            bli2.setIpVersion(IpVersion.IPV6);
+            bli2.setCidrBlock("2001:4100:7901::682a:3ae1::1/32");
+            bli.setIpVersion(IpVersion.IPV6);
+            bli.setCidrBlock("2001:4100:7908::682a:3ae1::1/32");
+            bl.getBlacklistItems().add(bli2);
+            Response response = blackListResource.addBlacklistItem(bl);
+            Assert.assertEquals(202, response.getStatus());
+        }
+
+        @Test
+        public void shouldReturn400WhenListUsingIPV6HasTwoDuplicates() {
+            bli2.setIpVersion(IpVersion.IPV6);
+            bli2.setCidrBlock("2001:4100:7901::682a:3ae1::1/32");
+            bli.setIpVersion(IpVersion.IPV6);
+            bli.setCidrBlock("2001:4100:7901::682a:3ae1::1/32");
+            bl.getBlacklistItems().add(bli2);
+            Response response = blackListResource.addBlacklistItem(bl);
+            Assert.assertEquals(400, response.getStatus());
+        }
+
     }
+
+    public static class whenRetrievingBlackList {
+
+        static final String mappingFile = "loadbalancing-dozer-management-mapping.xml";
+
+        BlackListResource blackListResource;
+
+        @Mock
+        BlacklistRepository blacklistRepository;
+
+        List<org.openstack.atlas.service.domain.entities.BlacklistItem> blacklistItemList;
+        org.openstack.atlas.service.domain.entities.BlacklistItem blacklistItem;
+
+        @Before
+        public void setUp() {
+            MockitoAnnotations.initMocks(this);
+            blackListResource = new BlackListResource();
+            blackListResource.setBlacklistRepository(blacklistRepository);
+            blackListResource.setMockitoAuth(true);
+            blacklistItemList = new ArrayList<>();
+            blacklistItem = new org.openstack.atlas.service.domain.entities.BlacklistItem();
+            blacklistItem.setId(1);
+            blacklistItem.setBlacklistType(org.openstack.atlas.service.domain.entities.BlacklistType.NODE);
+            blacklistItemList.add(blacklistItem);
+            blackListResource.setDozerMapper(DozerBeanMapperBuilder.create()
+                    .withMappingFiles(mappingFile)
+                    .build());
+
+            when(blacklistRepository.getAllBlacklistItems()).thenReturn(blacklistItemList);
+        }
+
+        @Test
+        public void shouldReturn200WhenRetrieving() {
+            Response response = blackListResource.retrieveBlacklist();
+            Assert.assertEquals(200, response.getStatus());
+        }
+
+        @Test
+        public void shouldReturn200WithEmptyList() {
+            blacklistItemList.clear();
+            Response response = blackListResource.retrieveBlacklist();
+            Assert.assertEquals(200, response.getStatus());
+        }
+
+    }
+
+    public static class whenCheckingIfNodeIsBlackList {
+
+        BlackListResource blackListResource;
+        @Mock
+        BlackListService blackListService;
+
+        Node node;
+
+        @Before
+        public void setUp() throws IpTypeMissMatchException, IPStringConversionException {
+            MockitoAnnotations.initMocks(this);
+            blackListResource = new BlackListResource();
+            blackListResource.setMockitoAuth(true);
+            blackListResource.setBlackListService(blackListService);
+        }
+
+        @Test
+        public void shouldReturn200WhenNodeIsBlackListed() throws IpTypeMissMatchException, IPStringConversionException {
+            when(blackListService.getBlackListedItemNode(any())).thenReturn(node);
+            Response response = blackListResource.isBlackListedNode("10.1.1.20");
+            Assert.assertEquals(200, response.getStatus());
+        }
+
+        @Test
+        public void shouldReturn200WhenNodeIsNotBlacklisted() throws IpTypeMissMatchException, IPStringConversionException {
+            when(blackListService.getBlackListedItemNode(any())).thenReturn(null);
+            Response response = blackListResource.isBlackListedNode("10.1.1.20");
+            Assert.assertEquals(200, response.getStatus());
+        }
+
+        @Test
+        public void shouldThrowIpStringConversionException() throws IPStringConversionException, IpTypeMissMatchException {
+            when(blackListService.getBlackListedItemNode(any())).thenThrow(IPStringConversionException.class);
+            Response response = blackListResource.isBlackListedNode("10.1.1.20");
+            Assert.assertEquals(500, response.getStatus());
+        }
+    }
+
 }
